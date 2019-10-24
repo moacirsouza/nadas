658e74c42d4ee2af2f48e90c967837c9df4e23de
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2019-10-23 22:01:58 -0300

    Arquivo HTML do template alterado para incluir quebras de linha (<br>) entre os links para a lista e o projeto

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/Gabarito/04/script.js
@@ -0,0 +1,19 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+var A, B, C, D, E, F, G, H, I, J;
+
+console.log("A <- 6*(3+2)");
+
+console.log("B <- 2+(6*(3+2))");
+console.log("C <- 2+(3*6)/(2+4)");
+console.log("D <- 2*(8/(3+1))");
+console.log("E <- 3+(16-2)/(2*(9-2))");
+console.log("G <- ((3+(8/2))*4)+(3*2)");
+console.log("F <- (6/3)+(8/2)");
+console.log("H <- (6*(3*3)+6)-10");
+console.log("I <- (((10*8)+3)*9)");
+console.log("J <- ((-12)*(-4))+(3*(-4))");
+
+console.log("\n");
+console.log("[-- FIM --]");
