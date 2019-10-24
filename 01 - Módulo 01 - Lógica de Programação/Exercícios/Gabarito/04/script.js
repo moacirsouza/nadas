61d176ea928169dd9468603adfc9a6b9f4030a22
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2019-10-23 22:27:18 -0300

    Mais gabaritos :D

--- 01 - Módulo 01 - Lógica de Programação/Exercícios/Gabarito/04/script.js
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/Gabarito/04/script.js
@@ -6,12 +6,19 @@ var A, B, C, D, E, F, G, H, I, J;
 console.log("Letra a) A <- 6*(3+2)");
 console.log("\tNão é possível remover os parênteses sem alterar o valor da expresão.\n\tProva:");
 A = 6*(3+2);
-console.log("A <- 6*(3+2): " + A);
+console.log("\tA <- 6*(3+2): " + A);
 A = 6*3+2;
-console.log("A <- 6*3+2: " + A);
+console.log("\tA <- 6*3+2: " + A);
 console.log("\n");
 
-console.log("B <- 2+(6*(3+2))");
+console.log("Letra b) B <- 2+(6*(3+2))");
+console.log("\tÉ possível remover os parênteses mais externos sem alterar o resultado.\n\tProva:")
+B = 2+(6*(3+2));
+console.log("\tB <- 2+(6*(3+2)): " + B);
+B = 2+6*(3+2);
+console.log("\tB <- 2+6*(3+2): " + B);
+console.log("\n");
+
 console.log("C <- 2+(3*6)/(2+4)");
 console.log("D <- 2*(8/(3+1))");
 console.log("E <- 3+(16-2)/(2*(9-2))");
