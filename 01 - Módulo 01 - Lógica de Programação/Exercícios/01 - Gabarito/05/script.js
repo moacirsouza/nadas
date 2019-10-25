91590e8f9ec8466bed4699445cf68b4a14eb6df2
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2019-10-25 08:37:05 -0300

    Resolução do Exercício 05

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/01 - Gabarito/05/script.js
@@ -0,0 +1,11 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+var numero;
+
+numero = Number(window.prompt("Digite um número: "));
+
+console.log("O número digitado foi: " + numero + " e o seu antecessor é: " + (numero-1));
+
+console.log("\n");
+console.log("[-- FIM --]");
