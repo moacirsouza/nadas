34ab6c05898964e9bb460fa14bfe121001c93cf1
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2019-10-25 19:51:40 -0300

    Resolução do Exercício 07

--- 01 - Módulo 01 - Lógica de Programação/Exercícios/01 - Gabarito/07/script.js
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/01 - Gabarito/07/script.js
@@ -1,18 +1,20 @@
 console.log("[-- INÍCIO --]");
 console.log("\n");
 
-var anos, meses, dias, umAno, umMes, idadeEmDias;
+var mensagem, nome, anos, meses, dias, umAno, umMes, idadeEmDias;
 
 umAno = 365;
 umMes = 30;
 
-anos = Number(window.prompt("Digite a sua idade em anos: "));
-meses = Number(window.prompt("Digite a sua idade em meses: "));
-dias = Number(window.prompt("Digite a sua idade em dias: "));
+nome = window.prompt("Olá! Vou calcular sua idade em dias. Mas primeiro, preciso saber qual é o seu nome:");
+window.alert("A seguir eu preciso saber há quantos anos, meses e dias você está vivo:");
+anos = Number(window.prompt("Anos: "));
+meses = Number(window.prompt("Meses: "));
+dias = Number(window.prompt("Dias: "));
 
 idadeEmDias = anos*umAno + meses*umMes + dias;
 
-console.log("A sua idade, em dias, é: " + idadeEmDias);
+console.log("Oi, " + nome + "!\n\nVocê já viveu " + idadeEmDias + " dias! Incrível!");
 
 console.log("\n");
 console.log("[-- FIM --]");
