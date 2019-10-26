38269f0c0208659ec6bc60cfaf66a1a12d3089d4
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2019-10-26 09:55:21 -0300

    Resolução do Exercício 08

--- 01 - Módulo 01 - Lógica de Programação/Exercícios/01 - Gabarito/08/script.js
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/01 - Gabarito/08/script.js
@@ -1,28 +1,44 @@
 console.log("[-- INÍCIO --]");
 console.log("\n");
 
-var eleitores, votosBrancos, votosNulos, votosValidos, totalDeVotos;
-var percentualDeVotosBrancos, percentualDeVotosNulos, percentualDeVotosValidos;
+/*
+Nota do Instrutor: Todas as variáveis abaixo poderiam ter sido definidas
+em apenas uma linha. A separação foi proposital, apenas para melhorar
+a legiblidade do código.
+*/
+var eleitores, votosBrancos, votosNulos, votosValidos, totalDeVotos, abstinencias;
+var percentualDeVotosBrancos, percentualDeVotosNulos;
+var percentualDeVotosValidos, percentualDeAbstinencias;
 
 eleitores    = Number(window.prompt("Informe o total de Eleitores do Município:"));
+votosValidos = Number(window.prompt("Informe o total de votos Válidos:"));
 votosBrancos = Number(window.prompt("Informe o total de votos Brancos:"));
 votosNulos   = Number(window.prompt("Informe o total de votos Nulos:"));
-votosValidos = Number(window.prompt("Informe o total de votos Válidos:"));
 
 totalDeVotos = votosBrancos + votosNulos + votosValidos;
+abstinencias = eleitores - totalDeVotos;
 
-percentualDeVotosBrancos = eleitores*votosBrancos/100;
-percentualDeVotosNulos = eleitores*votosNulos/100;
+percentualDeVotosBrancos = votosBrancos*100/eleitores;
+percentualDeVotosNulos = votosNulos*100/eleitores;
+percentualDeVotosValidos = votosValidos*100/eleitores;
+percentualDeAbstinencias = abstinencias*100/eleitores;
 
-console.log("+------------------------------------+");
-console.log("       [Resultado das Eleições]       ");
-console.log("+------------------------------------+");
-console.log("|  Votos  | Quantidade/Porcentagem |");
-console.log("+---------+------------+-------------+");
-console.log("| Válidos |  |");
-console.log("| Brancos |            | Porcentagem |");
-console.log("| Nulos   |            | Porcentagem |");
-console.log("+---------+------------+-------------+");
-
+console.log("+-----------------------------------------+");
+console.log("|            [Totalizadores]              |");
+console.log("+-----------------------------------------+");
+console.log("| Eleitores    | " + eleitores);
+console.log("| Votos        | " + totalDeVotos);
 console.log("\n");
-console.log("[-- FIM --]");
+
+console.log("+-----------------------------------------+");
+console.log("|      [Estatísticas das Eleições]        |");
+console.log("+-----------------------------------------+");
+console.log("|    Votos     |  Quantidade/Porcentagem  |");
+console.log("+--------------+--------------------------+");
+console.log("| Válidos      | " + votosValidos + "/" + percentualDeVotosValidos + "%");
+console.log("| Brancos      | " + votosBrancos + "/" + percentualDeVotosBrancos + "%");
+console.log("| Nulos        | " + votosNulos + "/" + percentualDeVotosNulos + "%");
+console.log("| Abstinências | " + abstinencias + "/" + percentualDeAbstinencias + "%");
+
+console.log("\n")
+console.log("[-- FIM--]");
