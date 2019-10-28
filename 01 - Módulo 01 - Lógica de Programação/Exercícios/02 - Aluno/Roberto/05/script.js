ed31192d8c07ecdaa81a8b25aeb869a3e0af57be
robertomendonca <robmen@gmail.com>
2019-10-28 10:49:22 -0300

    Resolução do Exercício 05

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02 - Aluno/Roberto/05/script.js
@@ -0,0 +1,9 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+var N; 
+N = Number(window.prompt("Digite o número: "))
+console.log ("O seu antecessor é: " + (N-1));
+
+console.log("\n");
+console.log("[-- FIM --]");
