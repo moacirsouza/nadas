de0dc664a3cab0f4a663cb44dc36fb66e13b0b4a
robertomendonca <robmen@gmail.com>
2019-11-03 10:29:42 -0300

    Resposta do Exercício 20

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02 - Aluno/Roberto/20/script.js
@@ -0,0 +1,16 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+var A,B
+
+A = Number(window.prompt("O valor do Primeiro Número é: "));
+B = Number(window.prompt("O valor do Segundo Número é: "));
+if (A>B){
+console.log("A ordem crescente dos valores informados é: " + B + " e " + A);
+}
+else{
+console.log("A ordem crescente dos valores informados é: " + A + " e " + B);
+}
+console.log("\n");
+console.log("[-- FIM --]");
+
