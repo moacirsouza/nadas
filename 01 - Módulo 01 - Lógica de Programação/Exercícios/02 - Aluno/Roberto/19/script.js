23e747e8a03ebea4117bef017f61adf9943ab1bc
robertomendonca <robmen@gmail.com>
2019-11-03 10:01:59 -0300

    Resposta do exerícicio 19

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02 - Aluno/Roberto/19/script.js
@@ -0,0 +1,12 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+var A,B
+A = Number(window.prompt("Digite o valor de A: "));
+B = Number(window.prompt("Digite o valor de B: "));
+if (A>B){
+console.log("O maior deles é: " + A);
+}
+
+console.log("\n");
+console.log("[-- FIM --]");
