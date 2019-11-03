569106f22f045bed13a4b5895f79aef5f3d2deb7
robertomendonca <robmen@gmail.com>
2019-11-02 23:25:19 -0300

    Resposta do Exercicio 14

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02 - Aluno/Roberto/14/script.js
@@ -0,0 +1,13 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+var numero
+numero = Number(window.prompt("Digite um valor: "));
+if(numero>10){
+console.log("O NÚMERO É MAIOR QUE 10");
+}
+else{
+console.log("O NÚMERO NÃO É MAIOR QUE 10");
+}
+console.log("\n");
+console.log("[-- FIM --]");
