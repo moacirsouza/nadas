4c4f0054a530679d6122b8ee20bc0de5b55d056a
robertomendonca <robmen@gmail.com>
2019-11-02 23:36:30 -0300

    Resposta do Exercício 15

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02 - Aluno/Roberto/15/script.js
@@ -0,0 +1,14 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+var valor
+valor = Number(window.prompt("Digite o valor: "));
+if (valor>=0){
+console.log("O VALOR É POSITIVO");
+}
+else{
+console.log("O VALOR É NEGATIVO");    
+}    
+
+console.log("\n");
+console.log("[-- FIM --]");
