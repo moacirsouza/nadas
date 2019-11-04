b40cacdaa2729ac7f9fc4e23a5987b6b2d0cfd0b
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2019-11-04 09:06:10 -0300

    Resolução dos exercícios 44 e 45. Rascunho do exercício 46.

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/01 - Gabarito/45/script.js
@@ -0,0 +1,17 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+var primeiroValor, segundoValor, resultadoDaDivisao;
+
+primeiroValor = Number(window.prompt("Digite o primeiro valor:"));
+segundoValor = Number(window.prompt("Digite o segundo valor (NÃO PODE SER ZERO):"));
+
+while(segundoValor == 0){
+    segundoValor = Number(window.prompt("Digite o segundo valor (NÃO PODE SER ZERO):"));
+}
+
+resultadoDaDivisao = primeiroValor/segundoValor;
+
+console.log("O valor da divisão entre " + primeiroValor + " e " + segundoValor + " é: " + resultadoDaDivisao);
+console.log("\n");
+console.log("[-- FIM --]");
---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/01 - Gabarito/46/script.js
@@ -0,0 +1,16 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+var primeiroValor, segundoValor, resultadoDaDivisao;
+
+primeiroValor = Number(window.prompt("Digite o primeiro valor:"));
+
+do{
+    segundoValor = Number(window.prompt("Digite o segundo valor (NÃO PODE SER ZERO):"));
+}while(segundoValor == 0);
+
+resultadoDaDivisao = primeiroValor/segundoValor;
+
+console.log("O valor da divisão entre " + primeiroValor + " e " + segundoValor + " é: " + resultadoDaDivisao);
+console.log("\n");
+console.log("[-- FIM --]");
