d176de2ea366acacf3a73cfa3b3f40a864d1f04a
robertomendonca <robmen@gmail.com>
2019-11-06 23:51:56 -0300

    Alteração no código

--- 01 - Módulo 01 - Lógica de Programação/Exercícios/02 - Aluno/Roberto/26/script.js
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02 - Aluno/Roberto/26/script.js
@@ -9,7 +9,7 @@ qtdemaximaemestoque = (window.prompt("Qtde máxima em estoque: "));
 console.log("Qtde máxima em estoque é: " + qtdemaximaemestoque);
 qtdeminimaemestoque = (window.prompt("Qtde mínima em estoque: "));
 console.log("Qtde mínima em estoque: " + qtdeminimaemestoque);
-qtdemedia = (qtdemaximaemestoque + qtdeminimaemestoque)/2
+qtdemedia = (qtdemaximaemestoque/2) + (qtdeminimaemestoque/2);
 console.log("A quantidade média em estoque é: " + qtdemedia);
 if (qtdeatualemestoque>=qtdemedia){
 console.log("Não efetuar a compra");
