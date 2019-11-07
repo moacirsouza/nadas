efc72021d6f849def2a4ecb06be132aa3015cc2b
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2019-11-06 22:14:44 -0300

    Resolução do Exercício 11

--- 01 - Módulo 01 - Lógica de Programação/Exercícios/01 - Gabarito/11/script.js
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/01 - Gabarito/11/script.js
@@ -7,10 +7,10 @@ vendedor = window.prompt("Informe o nome do vendedor:");
 percentualSobreValorTotalDeVendas = 0.05;
 proventosMensais = Number(window.prompt("Informe o salário de " + vendedor + ":"));
 totalDeVeiculosVendidos = Number(window.prompt("Informe a quantidade de carros vendidos por " + vendedor + ":"));
-comissaoPorVeiculo = Number(window.prompt("Informe o valor da comissão por carro de " + vendedor + ":"))
+comissaoPorVeiculo = Number(window.prompt("Informe, em Reais, o valor da comissão fixa por carro:"))
 valorTotalDasVendas = Number(window.prompt("Informe o valor total de vendas, em Reais, do vendedor " + vendedor + ":"));
 
-salarioFinal = proventosMensais + comissaoPorVeiculo*(1+totalDeVeiculosVendidos) + percentualSobreValorTotalDeVendas*(1+totalDeVeiculosVendidos);
+salarioFinal = proventosMensais + comissaoPorVeiculo*totalDeVeiculosVendidos + percentualSobreValorTotalDeVendas*valorTotalDasVendas;
 console.log("O salário final do vendedor " + vendedor + "é: " + salarioFinal);
 
 console.log("\n");
