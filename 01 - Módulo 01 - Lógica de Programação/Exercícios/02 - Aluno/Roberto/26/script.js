18faec15a6e642c185a402f7ac7d932c66384b6e
robertomendonca <robmen@gmail.com>
2019-11-06 23:16:14 -0300

    Exercício 26

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02 - Aluno/Roberto/26/script.js
@@ -0,0 +1,22 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+var qtdeatualemestoque, qtdemaximaemestoque, qtdeminimaemestoque, qtdemedia, qtdeatualemestoque
+
+qtdeatualemestoque = (window.prompt("Qtde atual em estoque: "));
+console.log("Qtde atual em estoque é: " + qtdeatualemestoque);
+qtdemaximaemestoque = (window.prompt("Qtde máxima em estoque: "));
+console.log("Qtde máxima em estoque é: " + qtdemaximaemestoque);
+qtdeminimaemestoque = (window.prompt("Qtde mínima em estoque: "));
+console.log("Qtde mínima em estoque: " + qtdeminimaemestoque);
+qtdemedia = (qtdemaximaemestoque + qtdeminimaemestoque)/2
+console.log("A quantidade média em estoque é: " + qtdemedia);
+if (qtdeatualemestoque>=qtdemedia){
+console.log("Não efetuar a compra");
+}
+else{
+console.log("Efetuar a compra");
+}
+
+console.log("\n");
+console.log("[-- FIM --]");
