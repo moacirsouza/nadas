9eaa4a5ef4b99697757c80fa8dfbea792e8cbee2
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2019-11-06 22:24:02 -0300

    Resolução do Exercício 12.

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/01 - Gabarito/12/script.js
@@ -0,0 +1,12 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+var celsius, fahrenheit;
+
+fahrenheit = Number(window.prompt("Informe o valor em Fahrenheit:"));
+celsius = 5/9*(fahrenheit-32);
+
+console.log("O valor de " + fahrenheit + "° Fahrenheit em Celsius é " + celsius + "°C.");
+
+console.log("\n");
+console.log("[-- FIM --]");
