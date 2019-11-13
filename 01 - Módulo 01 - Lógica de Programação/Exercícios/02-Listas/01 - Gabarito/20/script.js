1f0251f5955bdef0fedce82c7d9e7eeae2c3c41b
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2019-11-12 21:59:45 -0300

    Resolução do Exercício 20 da Lista 01

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/01 - Gabarito/20/script.js
@@ -0,0 +1,18 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+var primeiroValor, segundoValor, valoresEmOrdemCrescente;
+
+primeiroValor = Number(window.prompt("Informe o primeiro valor:"));
+segundoValor = Number(window.prompt("Informe o segundo valor:"));
+
+valoresEmOrdemCrescente = primeiroValor + ", " + segundoValor;
+
+if(segundoValor<primeiroValor){
+    valoresEmOrdemCrescente = segundoValor + ", " + primeiroValor;
+}
+
+console.log("Escrevendo os valores " + primeiroValor + " e " + segundoValor + ", em ordem crescente: " + valoresEmOrdemCrescente);
+
+console.log("\n");
+console.log("[-- FIM --]");
