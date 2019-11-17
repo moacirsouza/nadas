c7ee933b0b729d7f9eaa84bfd09efa440f796858
robertomendonca <robmen@gmail.com>
2019-11-16 22:22:58 -0300

    Feito o 30 !!

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/02 - Aluno/Roberto/30/script.js
@@ -0,0 +1,23 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+var n1,n2,n3
+
+n1 = Number(window.prompt("Digite o primeiro número: " ));
+console.log("O primeiro número é: " + n1);
+n2 = Number(window.prompt("Digite o segundo número: " ));
+console.log("O segundo número é: " + n2);
+n3 = Number(window.prompt("Digite o terceiro número: " ));
+console.log("O terceiro número é: " + n3);
+if ((n1>n3)&&(n2>n1)){
+    console.log("A ordem crescente é: " + n3 + "," + n1 + " e " + n2);
+    }
+    else if ((n1>n2)&&(n3>n1)){
+    console.log("A ordem crescente é: " + n2 + "," + n1 + " e " + n3);   
+    }
+    else {
+    console.log("A ordem crescente é: " + n1 + "," + n2 + " e " + n3);
+    }
+
+console.log("\n");
+console.log("[-- FIM --]");
