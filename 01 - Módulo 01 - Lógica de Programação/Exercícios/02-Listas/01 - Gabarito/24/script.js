ce5172310e1e6867568dd4c713591632e93516e8
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2019-12-14 14:42:35 -0300

    Criação da varivável baseParaCalculoDaComissao, a fim de dissociar esta informação do total de vendas. Pequenas alterações nos textos dos comentários, no intuito de melhorar a clareza do texto e finalização da lógica do cálculo de comissões.

--- 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/01 - Gabarito/24/script.js
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/01 - Gabarito/24/script.js
@@ -1,7 +1,7 @@
 console.log("[-- INÍCIO --]");
 console.log("\n");
 
-var vendedor, salarioFixo, totalDeVendas, tetoParaComissaoNivelUm, comissaoNivelUm, comissaoNivelDois, salarioFinal;
+var agenteDeVendas, salarioFixo, totalDeVendas, baseParaCalculoDaComissao, tetoParaComissaoNivelUm, comissaoNivelUm, comissaoNivelDois, salarioFinal;
 
 // Os três valores a seguir viraram variáveis ao invés de
 // serem inseridos diretamente no código. Esta é uma solução
@@ -16,26 +16,28 @@ comissaoNivelDois = 0.05;
 // durante o cálculo das comissões
 salarioFinal = 0;
 
-vendedor = window.prompt("Informe o nome do(a) vendedor(a):");
-salarioFixo = Number(window.prompt("Informe o salário fixo do(a) vendedor(a): " + vendedor));
-totalDeVendas = Number(window.prompt("Informe o valor total de vendas do(a) vendedor(a) " + vendedor + ":"));
+agenteDeVendas = window.prompt("Agente de Vendas:");
+salarioFixo = Number(window.prompt("Informe o salário fixo de " + agenteDeVendas + ":"));
+totalDeVendas = Number(window.prompt("Informe o valor total de vendas de " + agenteDeVendas + ":"));
+baseParaCalculoDaComissao = totalDeVendas;
 
 // Apenas se o total de vendas for maior que 1500 é que o
 // salário final recebe um acréscimo de 5% em cima da
 // diferença entre o total de vendas e o teto
 if(totalDeVendas > tetoParaComissaoNivelUm){
     salarioFinal = (totalDeVendas-tetoParaComissaoNivelUm)*comissaoNivelDois;
+    baseParaCalculoDaComissao = tetoParaComissaoNivelUm;
 }
 
-// Se a variável "salarioFinal" não fosse inicializada com 0,
-// todas as vezes que o total de vendas fosse menor ou igual
+// Se a variável "salarioFinal" não houvesse sido inicializada
+// com 0, todas as vezes que o total de vendas fosse menor ou igual
 // ao teto, o uso do "+=" retornaria "NaN" (Not a Number), pois
-// tentaria usar o valor de "salarioFinal", que neste momento seria
-// "undefined", no cálculo. Ao forçar aquela atribuição logo cedo no
-// código, somos capazes de evitar o uso de um "else" extra
-salarioFinal += salarioFixo + (totalDeVendas*comissaoNivelUm);
+// tentaria usar, no cálculo, o valor de "salarioFinal", que neste
+// momento seria "undefined". Ao forçar aquela atribuição logo cedo
+// no código, somos capazes de evitar o uso de um "else" extra.
+salarioFinal += salarioFixo + (baseParaCalculoDaComissao*comissaoNivelUm);
 
-console.log("O salário final do(a) vendedor(a) " + vendedor + " é de: " + salarioFinal);
+console.log("O salário final de " + agenteDeVendas + " é: " + salarioFinal);
 
 console.log("\n");
 console.log("[-- FIM --]");
