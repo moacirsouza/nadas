7c29690fd97b530d0f2f195012b929e322214e27
robertomendonca <robmen@gmail.com>
2019-12-27 21:31:36 -0300

    esse == me ferrou MOKA !!!!

--- 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/02 - Aluno/Roberto/35/script.js
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/02 - Aluno/Roberto/35/script.js
@@ -1,9 +1,34 @@
 console.log("[-- INÍCIO --]");
 console.log("\n");
 
-/*
-DIGITE AQUI O SEU CÓDIGO
-*/
+var precoA, precoG, numlitrosvendidos, tipodecombustivel, valoraserpago
+
+precoA = 2.90
+precoG = 3.30
+tipodecombustivel = window.prompt("Qual o tipo de combustível? ");
+console.log("O tipo de Combustível é: " + tipodecombustivel)
+numlitrosvendidos = Number(window.prompt("Qual o número de litros vendidos? "));
+console.log("O Número de litros vendidos foi: " + numlitrosvendidos);
+if (tipodecombustivel == "A"){
+    if(numlitrosvendidos > 20){
+    valoraserpago = (precoA*numlitrosvendidos)*0.95
+    console.log("O valor a ser pago é: " + valoraserpago)
+    }
+    else {
+        valoraserpago = (precoA * numlitrosvendidos)*0.97
+        console.log("O valor a ser pago é:" + valoraserpago)
+    }
+}
+else if (tipodecombustivel == "G"){
+    if(numlitrosvendidos > 20){
+    valoraserpago = (precoG*numlitrosvendidos)*0.94
+    console.log("O valor a ser pago é: " + valoraserpago)
+    }
+    else {
+    valoraserpago = (precoG*numlitrosvendidos)*0.96
+    console.log("O valor a ser pago é: " + valoraserpago)
+}
+}
 
 console.log("\n");
 console.log("[-- FIM --]");
