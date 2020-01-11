be0dd599967835f2ab07871d5076f3ad9e4746b1
robertomendonca <robmen@gmail.com>
2020-01-11 12:15:02 -0300

    one more

--- 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/02 - Aluno/Roberto/44/script.js
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/02 - Aluno/Roberto/44/script.js
@@ -4,13 +4,11 @@ console.log("\n");
 var v1, v2, divisao
 
 v1 = Number(window.prompt("Digite o primeiro valor: "))
-console.log("Primeiro valor: " + v1);
-v2 = Number(window.prompt("Digite o segundo valor: "))
-console.log("Segundo valor: " + v2);
-while (v2==0){  
-    console.log("DIGITE UM VALOR DIFERENTE DE ZERO");
-    v2 = Number(window.prompt("Digite o segundo valor: "))
-}
+
+do{
+    v2 = Number(window.prompt("Digite o segundo valor (NÃO PODE SER ZERO):"));
+    }while(v2 == 0);
+
 divisao = v1/v2
 console.log("O valor da divisão foi: " + divisao)
 
