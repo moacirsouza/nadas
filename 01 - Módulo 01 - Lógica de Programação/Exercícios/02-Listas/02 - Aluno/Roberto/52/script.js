630ef67be0ba844dc5e3d1ee8ce1e72c4dc43390
robertomendonca <robmen@gmail.com>
2020-01-11 16:16:10 -0300

    52 CONCLUÍDO

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/02 - Aluno/Roberto/52/script.js
@@ -0,0 +1,9 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+for(var contagem=101;contagem<111;contagem++){
+    console.log(+ contagem);
+}
+
+console.log("\n");
+console.log("[-- FIM --]");
