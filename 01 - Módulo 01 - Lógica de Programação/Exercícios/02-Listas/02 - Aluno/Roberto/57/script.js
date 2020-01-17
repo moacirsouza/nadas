f4d29fe90360033bbd8169b915a25e50cf2b2243
robertolopesmendonca <robmen@gmail.com>
2020-01-17 13:58:56 -0300

    .

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/02 - Aluno/Roberto/57/script.js
@@ -0,0 +1,13 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+contador = 0
+
+do{
+    contador = contador + 1
+    console.log(+ contador)
+    }while(contador <=9);
+
+
+console.log("\n");
+console.log("[-- FIM --]");
