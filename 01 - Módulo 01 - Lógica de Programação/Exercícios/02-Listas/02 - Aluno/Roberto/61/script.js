281174a6c50e4c40005b2a49131fdce6732f2116
robertomendonca <robmen@gmail.com>
2020-01-18 15:11:34 -0300

    61 Concluído

--- 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/02 - Aluno/Roberto/61/script.js
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/02 - Aluno/Roberto/61/script.js
@@ -1,9 +1,16 @@
 console.log("[-- INÍCIO --]");
 console.log("\n");
 
-/*
-DIGITE AQUI O SEU CÓDIGO
-*/
+var n
+somatorio = 0
+
+for (var contador=1; contador<=10; contador++){
+n = Number(window.prompt("Qual o valor? "));
+somatorio = somatorio + n
+}
+media = somatorio/10
+
+console.log("A média aritmética dos Dez valores lidos foi: " + media);
 
 console.log("\n");
 console.log("[-- FIM --]");
