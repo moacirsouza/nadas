80d8f27bd28173af9ba5f00bf3a48a830c974c66
robertomendonca <robmen@gmail.com>
2020-01-18 14:19:31 -0300

    Feito o 59

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/02 - Aluno/Roberto/59/script.js
@@ -0,0 +1,18 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+var  n
+q = 0
+
+for (var contador=1; contador<=10; contador++){
+n = Number(window.prompt("Qual o valor do número? "))
+if (n<0){
+    q = q + 1
+        }
+    }
+console.log("A quantidade de número(s) negativo(s) é(são): " + q);
+
+
+
+console.log("\n");
+console.log("[-- FIM --]");
