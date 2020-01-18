b70b369bfc70a28ab7d7cfe7713fa7d0e02b7357
robertomendonca <robmen@gmail.com>
2020-01-18 17:55:10 -0300

    Esse fumaçouuuuu Moka !!

--- 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/02 - Aluno/Roberto/65/script.js
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/02 - Aluno/Roberto/65/script.js
@@ -1,9 +1,18 @@
 console.log("[-- INÍCIO --]");
 console.log("\n");
 
-/*
-DIGITE AQUI O SEU CÓDIGO
-*/
+var valor1, valor2
+soma = 0
+
+valor1 = Number(window.prompt("Digite o Primeiro Valor "))
+valor2 = Number(window.prompt("Digite o Segundo Valor: "))
+for (var i=valor1; i<=valor2; i++){
+ soma = soma + i
+ }
+
+console.log("A soma dos valores entre os dois inteiros lidos é: " + soma);
+
+
 
 console.log("\n");
 console.log("[-- FIM --]");
