36a5786d1974c743770b48de35ddd7a9e4260a8d
robertomendonca <robmen@gmail.com>
2020-01-18 12:30:10 -0300

    Exercício 58 concluído

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/02 - Aluno/Roberto/58/script.js
@@ -0,0 +1,13 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+valor=11
+
+while ((valor>1) && (valor<=11)){  
+    valor = valor - 1
+    console.log(+ valor);
+    }
+    
+
+console.log("\n");
+console.log("[-- FIM --]");
