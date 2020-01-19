f31389eaf3186622968ab10c19a1f01ef7c9d376
robertomendonca <robmen@gmail.com>
2020-01-19 17:41:04 -0300

    Feito o 67

---
+++ 01 - Módulo 01 - Lógica de Programação/Exercícios/02-Listas/02 - Aluno/Roberto/67/script.js
@@ -0,0 +1,17 @@
+console.log("[-- INÍCIO --]");
+console.log("\n");
+
+soma = 0
+qtd = 0
+
+for (var i=15; i<=100; i++){
+    soma = soma + i
+    qtd = qtd + 1
+    }
+    media = soma/qtd
+
+console.log("A média aritmética é: " + media)
+
+
+console.log("\n");
+console.log("[-- FIM --]");
