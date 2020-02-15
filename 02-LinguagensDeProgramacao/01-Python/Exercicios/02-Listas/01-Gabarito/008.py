70a9e070972059e6abe036cab84db180f784e0e1
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-15 17:55:09 -0300

    Resolução do Exercício 008

--- 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/008.py
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/008.py
@@ -1,4 +1,18 @@
-print('[-- Escreva um programa que leia um valor e o exiba convertido em centímetros e milímetros --]\n')
-valorNumerico = float(input('Informe um valor em metros: '))
-valorEmCentimetros = valorNumerico*100
-valorEmMilimetros = valorNumerico*1000
+print('[-- Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros --]\n')
+metros = float(input('Informe um valor em metros: '))
+decimetros  = metros*10
+centimetros = metros*100
+milimetros  = metros*1000
+decametros  = metros/10
+hectometros = metros/100
+quilometros = metros/1000
+
+print('\n+-------------------------------------------+')
+print('| A medida informada em metros foi {}m'.format(metros))
+print('| Em decímetros, ela equivale a {}dm'.format(decimetros))
+print('| Em centrímetros, ela equivale a {}cm'.format(centimetros))
+print('| Em milímetros, ela equivale a {}mm'.format(milimetros))
+print('| Em decâmetros, ela equivale a {}dam'.format(decametros))
+print('| Em hectômetros, ela equivale a {}hm'.format(hectometros))
+print('| Em quilômetros, ela equivale a {}km'.format(quilometros))
+print('+-------------------------------------------+')
