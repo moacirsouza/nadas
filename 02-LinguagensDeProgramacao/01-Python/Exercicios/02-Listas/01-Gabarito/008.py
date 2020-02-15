5b277537a782eb1b102bf71fe323d125fffbc511
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-15 20:31:20 -0300

    Revisão da resolução do exercício 008, adicionando um conjunto mais complexo de formatadores à impressão dos valores.

--- 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/008.py
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/008.py
@@ -7,12 +7,12 @@ decametros  = metros/10
 hectometros = metros/100
 quilometros = metros/1000
 
-print('\n+-------------------------------------------+')
-print('| A medida informada em metros foi {}m'.format(metros))
-print('| Em decímetros, ela equivale a {}dm'.format(decimetros))
-print('| Em centrímetros, ela equivale a {}cm'.format(centimetros))
-print('| Em milímetros, ela equivale a {}mm'.format(milimetros))
-print('| Em decâmetros, ela equivale a {}dam'.format(decametros))
-print('| Em hectômetros, ela equivale a {}hm'.format(hectometros))
-print('| Em quilômetros, ela equivale a {}km'.format(quilometros))
-print('+-------------------------------------------+')
+print('\n+'+('-'*50)+'+')
+print('| A medida informada em metros foi: {:.> 12,.2f} m |'.format(metros))
+print('| Em decímetros, ela equivale a: {:.> 14,.2f} dm |'.format(decimetros))
+print('| Em centrímetros, ela equivale a: {:.> 12,.2f} cm |'.format(centimetros))
+print('| Em milímetros, ela equivale a: {:.> 14,.2f} mm |'.format(milimetros))
+print('| Em decâmetros, ela equivale a: {:.> 13,.2f} dam |'.format(decametros))
+print('| Em hectômetros, ela equivale a: {:.> 13,.2f} hm |'.format(hectometros))
+print('| Em quilômetros, ela equivale a: {:.> 13,.2f} km |'.format(quilometros))
+print('+'+('-'*50)+'+\n')
