dd9e5f26489514e846725143bb7a56e21a375d92
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-15 16:57:29 -0300

    Gabarito do Exercício 008

---
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/008.py
@@ -0,0 +1,4 @@
+print('[-- Escreva um programa que leia um valor e o exiba convertido em centímetros e milímetros --]\n')
+valorNumerico = float(input('Informe um valor em metros: '))
+valorEmCentimetros = valorNumerico*100
+valorEmMilimetros = valorNumerico*1000
