69033a125f8b7af898d923b40a5873073f508b36
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-15 20:35:28 -0300

    Revisão da resolução do exercício 005, removendo os espaços entre os operadores aritméticos em favor de melhorar a legibilidade do código.

--- 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/005.py
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/005.py
@@ -1,5 +1,5 @@
 print('[-- Desafio 005: Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e o seu antecessor. --]\n')
 numero=int(input('Digite um número: '))
-antecessor = numero - 1
-sucessor = numero + 1
+antecessor = numero-1
+sucessor = numero+1
 print('O antecessor do número {} é {}. Já o seu sucessor é {} '.format(numero,antecessor,sucessor))
