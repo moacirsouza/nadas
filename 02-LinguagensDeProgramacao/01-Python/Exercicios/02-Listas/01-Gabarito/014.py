ad4c791f2e03ab0a2b6f1e86fd5834a6a5bae015
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-16 22:27:27 -0300

    Resolução do Exercício 014

---
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/014.py
@@ -0,0 +1,4 @@
+print('[-- Escreva um programa que converta uma temperatura digitada em °C e converta para °F. --]\n')
+celsius = float(input('Digite a temperatura em Celsius (°C) para convertê-la em Fahrenheit (°F): '))
+fahrenheit = ((9*celsius)/5)+32 # Muitos parêntesis?
+print('{}°C equivalem a {}°F'.format(celsius,fahrenheit))
