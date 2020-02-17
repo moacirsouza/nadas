09a38e124e3ffee9b85cec9737bd6fbcdabd943c
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-16 22:39:24 -0300

    Resolução do Exercício 015.

---
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/015.py
@@ -0,0 +1,7 @@
+print('[-- Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0.15 por Km rodado. --]\n')
+diasDeLocacao = int(input('Por quantos dias o carro foi alugado? '))
+quilometrosRodados = float(input('Quantos quilômetros foram percorridos? '))
+custoDaLocacaoDiaria = 60
+custoPorQuilometroRodado = 0.15
+custoFinal = (diasDeLocacao*custoDaLocacaoDiaria) + (quilometrosRodados*custoPorQuilometroRodado)
+print('Um carro alugado por {} dia(s) e que rodou {} Km, custa um total de R$ {:.2f}'.format(diasDeLocacao,quilometrosRodados,custoFinal))
