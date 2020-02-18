24534a6da4544120dd05fdf5962eb6818dfb13d1
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-18 17:19:08 -0300

    Resolução do Exercício 021.

---
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/021.py
@@ -0,0 +1,4 @@
+print('[-- Faça um programa em Python que abra e reproduza o Áudio de um arquivo MP3. --]\n')
+from playsound import playsound
+musica = '/home/moacir/Downloads/essaPassou.mp3'
+playsound(musica)
