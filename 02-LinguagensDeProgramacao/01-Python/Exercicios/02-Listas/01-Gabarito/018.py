3c559f337d11ea9b3b1fbfe7d297ff99f84bb580
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-18 07:50:57 -0300

    Revisão da resolução do Exercício 018

--- 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/018.py
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/018.py
@@ -1,8 +1,8 @@
 print('[-- Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo. --]\n')
-from math import sin,cos,tan,degrees
+from math import sin,cos,tan,radians
 
-angulo = degrees(float(input('Informe o valor de um ângulo: ')))
-seno = sin(angulo)
-cosseno = cos(angulo)
-tangente = tan(angulo)
-print('Os valor de Seno, Cosseno e Tangente do ângulo {} são, respectivamente:\nSeno: {}\nCosseno: {}\nTangente: {}'.format(angulo, seno, cosseno, tangente))
+angulo = float(input('Informe o valor de um ângulo (Em graus): '))
+seno = sin(radians(angulo))
+cosseno = cos(radians(angulo))
+tangente = tan(radians(angulo))
+print('\nOs valores do seno, cosseno e tangente do ângulo {}° são, respectivamente: {:.2f}, {:.2f} e {:.2f}'.format(angulo, seno, cosseno, tangente))
