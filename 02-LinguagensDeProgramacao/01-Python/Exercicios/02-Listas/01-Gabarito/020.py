0652c73d62911f0f9b5f1c79866ad0951b3fecf5
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-18 17:05:19 -0300

    Resolução do Exercício 020.

---
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/020.py
@@ -0,0 +1,11 @@
+print('[-- O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada. --]\n')
+from random import shuffle
+nome01 = input('Informe o primeiro nome: ')
+nome02 = input('Informe o segundo nome: ')
+nome03 = input('Informe o terceiro nome: ')
+nome04 = input('Informe o quarto nome: ')
+
+alunos = [nome01,nome02,nome03,nome04]
+shuffle(alunos)
+
+print(alunos)
