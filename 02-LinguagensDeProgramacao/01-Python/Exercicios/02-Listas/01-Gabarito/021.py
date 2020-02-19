90127a799286f76f8e938798d1d35c7840fd0d92
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-19 08:14:26 -0300

    Revisão do Exercício 021: Adicionados mais dois métodos para reproduzir o aqruivo MP3.

--- 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/021.py
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/021.py
@@ -1,4 +1,24 @@
 print('[-- Faça um programa em Python que abra e reproduza o Áudio de um arquivo MP3. --]\n')
-from playsound import playsound
 musica = '/home/moacir/Downloads/essaPassou.mp3'
-playsound(musica)
+
+# Método 1, usando uma biblioteca externa: playsound
+# Para instalar essa biblioteca em seu ambiente, execute:
+# $ pip install playsound
+""" from playsound import playsound
+print('Para suspender a reprodução, pressione CTRL+Z')
+playsound(musica) """
+
+# Método 2, usando uma biblioteca externa: pygame
+# Para instalar essa biblioteca em seu ambiente, execute:
+# $ pip install pygame
+""" import pygame
+pygame.init()
+pygame.mixer_music.load(musica)
+pygame.mixer_music.play(-1) """
+
+# Método 3, usando um reprodutor instalado no S.O.
+# através de uma biblioteca interna (builtin): os
+""" import os
+print('Usando o "mplayer" para reproduzir o áudio.')
+print('Os comandos tradicionais do programa funcionam aqui.\nPressione "q" para suspender a reprodução')
+os.system('mplayer ' + musica) """
