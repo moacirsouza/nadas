add65f1cada1078e6a82ff5f1753f16d476e6714
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-19 13:21:17 -0300

    Revisão da resolução do Exercício 013: Adição de uma outra maneira de calcular um acréscimo de 15% de um valor.

--- 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/013.py
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/013.py
@@ -3,3 +3,8 @@ salario = float(input('Informe o salário do funcionário: R$ '))
 aumento = 0.15
 salarioFinal = salario+(salario*aumento)
 print('O salário final, com acréscimo de 15%, será R$ {:.2f}'.format(salarioFinal))
+
+### A fórmula a seguir é uma forma mais reduzida de calcular 15%
+### de aumento de qualquer valor
+""" salarioFinal = salario*1.15
+print('O salário final, com acréscimo de 15%, será R$ {:.2f}'.format(salarioFinal)) """
