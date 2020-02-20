88bef79babdb3f0bf7caa1d09ccf78bab6e2fa1f
aristontsfilho <aristontsfilho@gmail.com>
2020-02-20 11:42:41 -0300

    Exercicio do  5 ao 9

---
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/02-Aluno/Ariston/exc0006.py
@@ -0,0 +1,3 @@
+print('Ler um número e imprime o dobro, o triplo e a raiz quadrada')
+numero = int(input('Digite um numero: '))
+print('\nPara O numero {} \nO dobro é {}, \nO triplo é {} \nA raiz quadrada e {:.2f}.\n'.format(numero,numero*2,numero*3,numero**(1/2)))
