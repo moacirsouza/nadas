a558f171de719c2a36382b6fe7e3682c467fb10c
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-20 14:40:01 -0300

    Resolução do Exercício 023.

---
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/023.py
@@ -0,0 +1,1 @@
+print('[-- Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados. Ex: Digite um número: 1834. unidade: 4, dezena: 3, cententa: 8, milhar: 1. --]\n')
