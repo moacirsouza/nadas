2b621308bad3b592795d887f05027bbc2c87a6e8
robertolopesmendonca <robmen@gmail.com>
2020-02-20 16:07:49 -0300

    Na moral Moka, tu és Gigante Garoto !!!

---
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/02-Aluno/Roberto/exc0012.py
@@ -0,0 +1,6 @@
+print('[-- Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto. --]\n')
+preco = float(input('Qual o preço do produto? '))
+novopreco = preco*0.95
+print('O novo preço do produto é: {:.2f} ' .format(novopreco))
+
+
