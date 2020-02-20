96ca87f49d8d25c02a6ff914249ee42a323aef4d
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-20 07:13:25 -0300

    Movendo o diretório Roberto para dentro da área de alunos.

--- 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/000-Template.py
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/000-Template.py
@@ -1,6 +1,6 @@
 print('[--  --]\n')
 
-022. Crie um programa que leia o nome completo de uma pessoa e mostre: a) O nome com todas as letras maiúsculas; b) O nome com todas as letras minúsculas; c) Quantas letras ao todo (sem considerar espaços); d) Quantas letras tem o primeiro nome
+""" 022. Crie um programa que leia o nome completo de uma pessoa e mostre: a) O nome com todas as letras maiúsculas; b) O nome com todas as letras minúsculas; c) Quantas letras ao todo (sem considerar espaços); d) Quantas letras tem o primeiro nome
 
 023. Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados. Ex: Digite um número: 1834. unidade: 4, dezena: 3, cententa: 8, milhar: 1.
 
@@ -10,4 +10,4 @@ print('[--  --]\n')
 
 026. Faça um programa que leia uma frase pelo teclado e mostre: a) Quantas vezes aparece a letra "A"; b) Em que posição ela aparece a primeira vez; c) Em que posição ela aparece a última vez.
 
-027. Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente. Ex: Ana Maria de Souza. primeiro=Ana, último: Souza.
+027. Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente. Ex: Ana Maria de Souza. primeiro=Ana, último: Souza. """
