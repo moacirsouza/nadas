959cc165f0a88b8091d9507ddc3a179adf3ccabf
aristontsfilho <aristontsfilho@gmail.com>
2020-02-21 07:13:59 -0300

    Exercicio 16 e 17

---
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/02-Aluno/Ariston/exc0016.py
@@ -0,0 +1,4 @@
+print('Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.\n')
+numero = float(input('Digite um número real: '))
+Inteiro = int(numero)
+print("A parte inteira do número {} é {}".format(numero,Inteiro))
