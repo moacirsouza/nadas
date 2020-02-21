049a4e35bf8a1ed75b2cdc8caabe2fe596c41736
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-21 07:56:59 -0300

    Revisão da resolução do Exercício 024: Separação das técnicas de busca dentro da string e da apresentação. Adição do método strip(), que resolve o problema de iniciar o texto com um ou mais espaços em branco.

--- 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/024.py
+++ 02-LinguagensDeProgramacao/01-Python/Exercicios/02-Listas/01-Gabarito/024.py
@@ -1,3 +1,4 @@
 print('[-- Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO". --]\n')
 cidade = input('Digite o nome da cidade (Maiúsculas e Minúsculas não serão diferenciadas): ')
-print('\nA cidade começa com a palavra SANTO? -> {}\n'.format(cidade.lower().startswith('santo')))
+buscaPeloSanto = cidade.lower().strip().startswith('santo')
+print('\nA cidade começa com a palavra SANTO? -> {}\n'.format(buscaPeloSanto))
