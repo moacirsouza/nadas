15ea9ba325ea0f03d1d5bc778e46a490812d2290
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-24 17:42:29 -0300

    Revisão da resolução do exercício 025: Utilização de uma variável intermediária para realizar o pré-processamento da entrada e facilitar a verificação das informações solicitadas no enunciado.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/025.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/025.py
@@ -3,19 +3,29 @@ print('[-- Crie um programa que leia o nome de uma pessoa e diga se ela tem "SIL
 ### Entrada do programa: Recebe o nome de uma pessoa como "string"
 nome = str(input('Digite seu nome completo: '))
 
-### A variável "ondeEstaSilva" realiza uma série de procedimentos na 
-### "string", a fim de minimizar os potenciais problemas que o
+### A variável "nomeProcessado" realiza uma série de procedimentos
+### na "string", a fim de minimizar os potenciais problemas que o
 ### usuário pode causar na hora de verificar a existência do 
 ### padrão na entrada. O método "strip()" é usado para remover
 ### espaços em branco do começo e do final do texto, enquanto o
 ### "lower()" força todos os caracteres a ficarem em minúsculo,
-### facilitando a verificação do "find()".
-ondeEstaSilva = nome.strip().lower().find('silva')
+### facilitando, por exemplo, a verificação do "find()".
+nomeProcessado = nome.lower().strip()
 
-### Similarmente à variável "ondeEstaSilva", a "temSilva" realiza
-### procedimentos que facilitam as verificações do operador "in"
-### do Python.
-temSilva = nome.strip().lower()
+### Depois de processar a entrada na variável "nomeProcessado", a
+### utilização de métodos como o "find()" fica amplamente simplificada.
+### Agora, independente de como a entrada for entregue ao programa,
+### i.e., usando letras maiúsculas ou minúsculas, com o sem espaços no
+### começo ou no final do nome, o "find()" não terá problemas em
+### encontrar o padrão.
+ondeEstaSilva = nomeProcessado.find('silva')
+
+### Similarmente à variável "ondeEstaSilva", a "temSilva" deve
+### realizar procedimentos que facilitam as verificações do operador
+### "in" do Python. Como esses procedimentos foram realizados na 
+### variável "nomeProcessado", é suficiente fazer "temSilva" receber
+### o valor de "nomeProcessado".
+temSilva = nomeProcessado
 
 print('Método 1: Usando o método "find()": Se houver "Silva", mostre em que posição ele aparece. Se não houver, imprima "-1": {}'.format(ondeEstaSilva))
 print('Método 2: Usando o operador "in": Existe "Silva" no nome? -> {}'.format('silva' in temSilva))
