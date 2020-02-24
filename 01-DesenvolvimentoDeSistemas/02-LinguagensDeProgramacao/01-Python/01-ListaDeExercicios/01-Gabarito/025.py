00101dfcf5ffbe55f4388bdd9921dc34c2df410a
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-24 15:32:23 -0300

    Revisão da resolução do Exercício 025.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/025.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/025.py
@@ -3,13 +3,19 @@ print('[-- Crie um programa que leia o nome de uma pessoa e diga se ela tem "SIL
 ### Entrada do programa: Recebe o nome de uma pessoa como "string"
 nome = str(input('Digite seu nome completo: '))
 
-### A variável "temSilva" realiza uma série de procedimentos na 
+### A variável "ondeEstaSilva" realiza uma série de procedimentos na 
 ### "string", a fim de minimizar os potenciais problemas que o
 ### usuário pode causar na hora de verificar a existência do 
-### padrão na entrada. Os métodos "strip()" é usado para remover
+### padrão na entrada. O método "strip()" é usado para remover
 ### espaços em branco do começo e do final do texto, enquanto o
-### "lower()" força todos os caracteres alfabéticos a ficarem em
-### minúsculo, facilitando a verificação do "find()".
-temSilva = nome.strip().lower().find('silva')
+### "lower()" força todos os caracteres a ficarem em minúsculo,
+### facilitando a verificação do "find()".
+ondeEstaSilva = nome.strip().lower().find('silva')
 
-print('O nome informado tem "Silva"? Se sim, em que posição? Se não, a resposta será "-1".\n{}'.format(temSilva))
+### Similarmente à variável "ondeEstaSilva", a "temSilva" realiza
+### procedimentos que facilitam as verificações do operador "in"
+### do Python.
+temSilva = nome.strip().lower()
+
+print('Método 1: Usando o método "find()": Se houver "Silva", mostre em que posição ele aparece. Se não houver, imprima "-1": {}'.format(ondeEstaSilva))
+print('Método 2: Usando o operador "in": Existe "Silva" no nome? -> {}'.format('silva' in temSilva))
