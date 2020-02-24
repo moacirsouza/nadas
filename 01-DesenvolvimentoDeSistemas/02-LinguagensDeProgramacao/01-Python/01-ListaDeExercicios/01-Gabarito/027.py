48c5a785c6463f5853ae924ccaa429e61656e493
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-24 17:34:50 -0300

    Criação do template para a resolução do Exercício 027.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/027.py
@@ -0,0 +1,1 @@
+print('[-- Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente. Ex: Ana Maria de Souza. primeiro=Ana, último: Souza. --]\n')
