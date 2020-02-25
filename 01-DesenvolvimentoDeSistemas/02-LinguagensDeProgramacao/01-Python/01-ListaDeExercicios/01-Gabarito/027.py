9a914b00e7f1a9dc0855c86e385078cc48329866
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-25 10:12:31 -0300

    Revisão da resolução do Exercício 027: Adição do método alternativo para encontrar o último item de uma lista e alteração do formato do enunciado.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/027.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/027.py
@@ -1,4 +1,9 @@
-print('[-- Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente. Ex: Ana Maria de Souza. primeiro=Ana, último: Souza. --]\n')
+print("""
+027) Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+Ex: Ana Maria de Souza.
+primeiro=Ana
+último: Souza.
+""")
 
 ### Entrada do programa: Recebe o nome da pessoa como "string"
 nome = str(input('Digite o seu nome completo: '))
@@ -10,12 +15,21 @@ nomeProcessado = nome.strip()
 ### O método "split()" divide uma "string" em uma lista de
 ### de "strings", separadas por espaço. Por exemplo: Se a
 ### "string" 'um texto longo' sofrer um "split()", o resultado
-### será a lista ['um','texto','longo']. Cada um dos itens poderá,
+### será a lista ['um', 'texto',' longo']. Cada um dos itens poderá,
 ### então, ser referenciado por um índice numérico que começa
 ### em 0. Em qualquer lista há um índice especial, "-1", que sempre
 ### se refere ao último item do conjunto.
 primeiroNome = nomeProcessado.split()[0]
 ultimoNome = nomeProcessado.split()[-1]
+
+### Uma maneira alternativa para encontrar o último item de uma lista
+### é calcular o tamanho dela com o método "len()" e informar este
+### valor, subtraído de 1, como índice. A subtração é necessária porque
+### o "len()" retorna o tamanho da lista como se ela começasse com o
+### índice 1. Por exemplo, o tamanho da lista ['um', 'texto', 'longo']
+### é 3, mas o índice do último item é 2. Sem a subtração, recebe-se
+### o erro "IndexError: list index out of range".
+# ultimoNome = nomeProcessado.split()[len(nomeProcessado.split())-1]
 
 print("""
 O seu primeiro nome é: {}.
