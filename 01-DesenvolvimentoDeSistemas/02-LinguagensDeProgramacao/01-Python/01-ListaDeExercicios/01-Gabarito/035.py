ee2fdcbf82e1d68823f88006f33c8ff8178ef0f4
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-27 14:24:33 -0300

    Template básico da resolução do exercício 035.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/035.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/035.py
@@ -3,5 +3,20 @@ print("""
 elas podem ou não formar um triângulo.
 """)
 
-### A soma de quaisquer dois lados de um triângulo deve, SEMPRE,
-### ser maior que o terceiro lado restante.
+print("""
+Este programa recebe o valor de três retas e verifica se, com elas,
+é possível formar um triângulo. Para isso é importante saber que a
+a soma de quaisquer dois lados de um triângulo deve, SEMPRE,
+ser maior que o terceiro lado restante.
+""")
+
+lado01 = float(input('Digite o valor do lado 01: '))
+lado02 = float(input('Digite o valor do lado 02: '))
+lado03 = float(input('Digite o valor do lado 03: '))
+
+if lado01+lado02 > lado03:
+    if lado01+lado03 > lado02:
+        if lado02+lado03 > lado01:
+            print('É triângulo')
+else:
+    print('Não é.')
