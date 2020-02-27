fc2b18e09d60b4819a3c23e30e1f9b6597de83f0
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-26 22:13:31 -0300

    Revisão da resolução do Exercício 033: Adicionadas as verificações de menor valor, que faltaram na primeira versão.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/033.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/033.py
@@ -12,19 +12,33 @@ numero03 = int(input('Digite o terceiro número: '))
 ### maior, pois essa posição é temporária e os testes a seguir é que
 ### vão definir qual das entradas possui o maior valor.
 maior = numero01
+menor = numero01
 
-### Se o segundo número é maior que o maior valor atual, a variável
-### "maior" recebe este novo valor.
-if numero02 > maior:
+### Testes para verificação do maior número. Se os quatro testes
+### falharem, o maior número será o primeiro, conforme a primeira
+### atribuição da variável "maior" supôs.
+if numero02 > numero01 and numero02 > numero03:
     maior = numero02
 
-### Da mesma forma que no primeiro teste, o terceiro número é comparado
-### com o valor atualmente retido na variável "maior" e, se ele for maior,
-### a variável "maior" termina com este valor.
-if numero03 > maior:
+if numero03 > numero01 and numero03 > numero02:
     maior = numero03
 
+### Testes para verificação do menor número. Se os quatro testes
+### falharem, o menor número será o primeiro, conforme a primeir
+### atribuição da variável "menor" supôs.
+if numero02 < numero01 and numero02 < numero03:
+    menor = numero02
+
+if numero03 < numero01 and numero03 < numero02:
+    menor = numero03
+    
 ### Não importa qual das três variáveis recebe o maior valor, os testes
 ### vão sempre garantir que esse número seja, eventualmente, atribuído
-### à variável "maior"
-print('\nO maior número entre {}, {} e {} é: {}'.format(numero01, numero02, numero03, maior))
+### à variável "maior". O mesmo raciocínio vale para o menor valor.
+print("""
+Os três números informados foram: {}, {} e {}.
+O maior deles é {}.
+O menor deles é {}.
+""".format(numero01, numero02, numero03, maior, menor))
+
+### E se dois dos três números forem iguais?
