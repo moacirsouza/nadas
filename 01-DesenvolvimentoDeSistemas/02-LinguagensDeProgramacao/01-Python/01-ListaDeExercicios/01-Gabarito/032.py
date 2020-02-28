0b7cc56243773e25a3f116986447630b9965c743
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-02-28 12:08:44 -0300

    Revisão da resolução do Exercício 032: Adição de dois métodos diferentes para resolver o problema. O primeiro transpõe o pseudocódigo da Wikipedia diretamente e o segundo, resolve tudo dentro de um único if.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/032.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/032.py
@@ -9,14 +9,30 @@ ano = int(input('Informe o número do ano para saber se ele é bissexto: '))
 ### Wikipedia sobre ano bissexto. Nele é possível entender quais as
 ### verificações básicas para avaliar se um ano é bissexto ou não:
 ### https://en.wikipedia.org/wiki/Leap_year#Algorithm
-# if (year is not divisible by 4) then (it is a common year)
-# else if (year is not divisible by 100) then (it is a leap year)
-# else if (year is not divisible by 400) then (it is a common year)
-# else (it is a leap year)
+### 
+### if (year is not divisible by 4) then (it is a common year)
+### else if (year is not divisible by 100) then (it is a leap year)
+### else if (year is not divisible by 400) then (it is a common year)
+### else (it is a leap year)
 
 ### O conceito matemático de divisibilidade é usado para realizar
 ### os testes deste programa. Um número é divisível por outro 
 ### quando o resto da divisão entre eles é zero.
+
+### Método 1: Transpondo o pseudocódigo exatamente como ele foi descrito.
+# comum = 'Ano comum'
+# bissexto = 'Ano Bissexto'
+
+# if ano%4 != 0:
+#     print(comum)
+# elif ano%100 != 0:
+#     print(bissexto)
+# elif ano%400 != 0:
+#     print(comum)
+# else:
+#     print(bissexto)
+
+### Método 2: Usando apenas um "if".
 if ano%4 != 0 or ano%400 != 0 and ano%100 == 0:
     print('{} é um ano comum.'.format(ano))
 else:
