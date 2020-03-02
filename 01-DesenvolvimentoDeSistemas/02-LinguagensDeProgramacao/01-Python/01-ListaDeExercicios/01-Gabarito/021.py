164b08af8067067b72cc67a005ba945f5e7ed9ee
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-03-02 09:10:20 -0300

    Revisão da resolução do Exercício 021: Alteração dos comentários com aspas duplas triplas por cerquilha (Como deve ser).

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/021.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/021.py
@@ -4,9 +4,9 @@ musica = '/home/moacir/Downloads/essaPassou.mp3'
 ### Método 1, usando uma biblioteca externa: playsound
 ### Para instalar essa biblioteca em seu ambiente, execute:
 ### $ pip install playsound
-""" from playsound import playsound
-print('Para suspender a reprodução, pressione CTRL+Z')
-playsound(musica) """
+# from playsound import playsound
+# print('Para suspender a reprodução, pressione CTRL+Z')
+# playsound(musica)
 
 ### TODO: Este método não está funcionando. É preciso estudar
 ### as modificações recentes no pygame para entender como o
@@ -14,14 +14,14 @@ playsound(musica) """
 ### Método 2, usando uma biblioteca externa: pygame
 ### Para instalar essa biblioteca em seu ambiente, execute:
 ### $ pip install pygame
-""" import pygame
-pygame.init()
-pygame.mixer_music.load(musica)
-pygame.mixer_music.play(-1) """
+# import pygame
+# pygame.init()
+# pygame.mixer_music.load(musica)
+# pygame.mixer_music.play(-1)
 
 ### Método 3, usando um reprodutor instalado no S.O.
 ### através de uma biblioteca interna (builtin): os
-""" import os
-print('Usando o "mplayer" para reproduzir o áudio.')
-print('Os comandos tradicionais do programa funcionam aqui.\nPressione "q" para suspender a reprodução')
-os.system('mplayer ' + musica) """
+# import os
+# print('Usando o "mplayer" para reproduzir o áudio.')
+# print('Os comandos tradicionais do programa funcionam aqui.\nPressione "q" para suspender a reprodução')
+# os.system('mplayer ' + musica)
