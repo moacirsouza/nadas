c040c1ef3cb939d7f818d8e152e08761d8ba5343
aristontsfilho <aristontsfilho@gmail.com>
2020-03-03 12:33:28 -0300

    Exercício 20 com random.shuffle

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Ariston/exc0020.py
@@ -0,0 +1,12 @@
+print("""
+020) Crie um programa em PYTHON que: O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos
+dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+""")
+from random import shuffle
+aluno01 = str(input('Digite o nome do Aluno 01: '))
+aluno02 = str(input('Digite o nome do Aluno 02: '))
+aluno03 = str(input('Digite o nome do Aluno 03: '))
+aluno04 = str(input('Digite o nome do Aluno 04: '))
+classe = [aluno01,aluno02,aluno03,aluno04]
+shuffle(classe)
+print('\nA orde escolhida para apresentar o trabalho foi {}'.format(classe))
