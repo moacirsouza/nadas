e55bb058ff0fd7d2418bc6cdbc588847dca644c1
aristontsfilho <aristontsfilho@gmail.com>
2020-03-04 08:00:28 -0300

    Exercício 32 bissexto

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Ariston/exc0032.py
@@ -0,0 +1,12 @@
+print("""
+032) Crie um programa em PYTHON que: leia um ano qualquer e moste se ele é BISSEXTO.
+""") 
+### if (year is not divisible by 4) then (it is a common year)
+### else if (year is not divisible by 100) then (it is a leap year)
+### else if (year is not divisible by 400) then (it is a common year)
+### else (it is a leap year)
+ano = int(input('Digite o Ano: '))
+if ano%4!=0 or ano%400!=0 and ano%100==0:
+    print('\nO ano é {} é Comum'.format(ano))
+else:
+    print('\nO ano é {} é Bissexto'.format(ano))
