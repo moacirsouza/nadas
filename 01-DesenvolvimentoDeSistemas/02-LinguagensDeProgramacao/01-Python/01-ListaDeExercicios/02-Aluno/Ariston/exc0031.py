ac5f029f4748264515b29b3ee91a3a1dec42ea31
aristontsfilho <aristontsfilho@gmail.com>
2020-03-04 07:12:09 -0300

    Exercício 31 if else

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Ariston/exc0031.py
@@ -0,0 +1,12 @@
+print("""
+031) Crie um programa em PYTHON que:pergunte a distância de uma viagem em Km. Calcule o preço da
+passagem, cobrando R$0,50 por Km para viagens até 200Km e R$0,45 para viagens mais longas.
+""") 
+distancia = float(input('Qual a distancia da viagem: '))
+limite = 200
+curta = 0.5
+longa = 0.45
+if distancia > limite:
+    print('\nO valor da passagem é R$ {:.2f}'.format(distancia*longa))
+else:
+    print('\nO valor da passagem é R$ {:.2f}'.format(distancia*curta))
