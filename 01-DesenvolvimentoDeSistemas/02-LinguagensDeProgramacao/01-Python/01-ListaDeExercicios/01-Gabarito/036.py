6f9bfcf8ed21823682588a07d1d27dd53f1a8084
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-03-07 13:18:20 -0300

    Revisão da resolução do Exercício 036: Separação das variáveis e organização do código em seções, a fim de melhorar a legibilidade

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/036.py
@@ -0,0 +1,48 @@
+print("""
+036) Escreva um programa para aprovar um empréstimo bancário para a compra de uma casa.
+O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele
+vai pagar.
+
+Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou
+então o empréstimo será negado.
+""")
+
+### Entrada do programa: Recebe o valor da casa e o salário do comprador emem real
+### (número real, "float") e o tempo de pagamento como inteiro ("int").
+valorDoImovel = float(input('Informe o valor do imóvel: R$ ').strip())
+salarioDoComprador = float(input('Informe o salário do(a) comprador(a): R$ ').strip())
+tempoDePagamento = int(input('Informe o tempo de pagamento (Em anos): ').strip())
+
+percentualDoSalario = 0.3
+limiteSuperior = salarioDoComprador*percentualDoSalario
+parcelaMensal = valorDoImovel/(tempoDePagamento*12)
+mensagemDeAprovacao = """
+Parabéns! Seu financiamento foi aprovado!
+Veja os detalhes financeiros abaixo.
+"""
+situacaoDoFinanciamento = 'APROVADO'
+
+if parcelaMensal > limiteSuperior:
+    mensagemDeAprovacao = """
+Sinto muito. Seu financiamento foi negado.
+Veja os detalhes financeiros abaixo.
+"""
+    situacaoDoFinanciamento = 'NEGADO'
+
+print(mensagemDeAprovacao.format(parcelaMensal))
+print("""
+Resumo financeiro:
+- Valor do Imóvel: R$ {:.2f}
+- Salário do Comprador: R$ {:.2f}
+- Tempo de pagamento: {} anos
+- Valor da Parcela: R$ {:.2f}
+- Limite Superior (30% do Salário): R$ {:.2f}
+- Situação do Financiamento: {}
+""".format(
+    valorDoImovel,
+    salarioDoComprador,
+    tempoDePagamento,
+    parcelaMensal,
+    limiteSuperior,
+    situacaoDoFinanciamento
+    ))
