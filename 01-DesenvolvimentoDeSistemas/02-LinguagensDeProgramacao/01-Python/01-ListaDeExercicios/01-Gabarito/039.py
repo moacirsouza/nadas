de20186e59f27fbb6312ec80f598dfd874d44875
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-03-12 17:23:19 -0300

    Esboço da resolução do Exercício 039.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/039.py
@@ -0,0 +1,8 @@
+print("""
+039) Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo
+com a sua idade, se ele ainda vai se alistar ao serviço, se é a hora de se alistar ou
+se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que
+falta ou que passou do prazo.
+""")
+
+idade = int(input('Informe a idade do jovem: ').strip())
