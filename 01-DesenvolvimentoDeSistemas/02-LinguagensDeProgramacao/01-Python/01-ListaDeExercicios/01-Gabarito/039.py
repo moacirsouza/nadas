85b618e0b49240dbd475e966239abc5df3756618
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-03-17 23:01:35 -0300

    Revisão da resolução do Exercício 039: Finalização do programa e correções textuais nos comentários.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/039.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/039.py
@@ -5,4 +5,45 @@ se já passou do tempo do alistamento. Seu programa também deverá mostrar o te
 falta ou que passou do prazo.
 """)
 
-idade = int(input('Informe a idade do jovem: ').strip())
+### Importação da classe "date", do módulo "datetime"
+from datetime import date
+
+### Entrada do programa: A variável "anoDeNascimento" recebe o valor
+### de entrada como inteiro (int). Novamente, é importante notar o uso
+### do método "strip()", no momento do recimento da entrada do usuário,
+### a fim de fazer uma primeira validação simples, i.e., a remoção de
+### possíveis espaços em branco antes e/ou depois do conteúdo que será
+### processado
+anoDeNascimento = int(input('Informe o ano em que você nasceu: ').strip())
+
+### A propriedade "year", do construtor "today()", retorna o valor
+### do ano atual
+anoAtual = date.today().year
+
+idadeDoJovem = anoAtual - anoDeNascimento
+
+mensagem = """
+Estamos em {} e você tem, ou terá até o fim do ano, {} ano(s).
+""".format(anoAtual, idadeDoJovem)
+
+### O conjunto de testes é bem simples e apenas verifica se o jovem
+### tem mais, menos ou exatamente 18 anos de idade. De acordo com
+### este critério a variável "mensagem" recebe novas informações para
+### cada caso.
+if idadeDoJovem > 18:
+    prazo = idadeDoJovem - 18
+    mensagem += 'O prazo para seu alistamento já expirou há {} ano(s).\n'.format(prazo)
+elif idadeDoJovem < 18:
+    prazo = 18 - idadeDoJovem
+    mensagem += 'Ainda falta(m) {} ano(s) para o seu alistamento.\n'.format(prazo)
+else:
+    mensagem += """
+Este é o ano do seu alistamento! Para maiores
+informações, visite a Junta Militar mais próxima!
+"""
+
+### Por fim, apenas a variável "mensagem" é passada como parâmetro
+### para a saída do programa. Todos os dados, cálculos e variações
+### possíveis, derivadas da regra de negócios, já foram tratadas
+### anteriormente, por isso não é mais preciso alterar nada pontualmente
+print(mensagem)
