3cabf8a791e227eba68bdea879d8d68b4a4ea2da
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-03-21 07:18:46 -0300

    Resolução do Exercício 040.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/040.py
@@ -0,0 +1,30 @@
+print("""
+040) Crie um programa que leia duas notas de umaluno e calcule sua média, mostrando
+uma mensagemFinal no final, de acordo com a média atingida:
+- Média abaixo de 5.0: REPROVADO
+- Média entre 5.0 e 6.9: RECUPERAÇÃO
+- Média 7.0 ou superior: APROVADO
+""")
+
+nota01 = float(input('Informe a primeira nota do(a) aluno(a): ').strip())
+nota02 = float(input('Informe a segunda nota do(a) aluno(a): ').strip())
+
+media = (nota01+nota02)/2
+
+if media < 5:
+    resultado = 'REPROVADO(A)'
+elif media >= 5 and media <= 6.9:
+    resultado = 'em RECUPERAÇÃO'
+else:
+    resultado = 'APROVADO(A)'
+
+mensagemFinal = """
+As notas do(a) aluno(a) foram {} e {}.
+A média do(a) aluno(a) é {:.2f}.
+Ele(a) está {}
+""".format(nota01,
+           nota02,
+           media,
+           resultado)
+
+print(mensagemFinal)
