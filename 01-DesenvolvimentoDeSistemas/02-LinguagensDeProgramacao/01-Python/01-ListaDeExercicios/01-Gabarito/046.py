821aecb8dca776e8dc7bced0dbedb1528fee405b
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-03-24 19:52:05 -0300

    Resolução do Exercício 046.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/046.py
@@ -0,0 +1,11 @@
+print("""
+046) Faça um programa que mostre na tela uma contagem regressiva para o estouro de
+fogos de artifício, indo de 10 até 0, com pausa de 1 segundo entre eles.
+""")
+
+from time import sleep
+
+for contador in range(10, -1, -1):
+    print(contador)
+    sleep(0.5)
+print('POW! POW! BUM! CATAPLAM!')
