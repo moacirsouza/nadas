24e57be7f1498a08296d6714d80e248b1a729654
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-03-25 11:00:13 -0300

    Resolução do Exercício 047.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/047.py
@@ -0,0 +1,11 @@
+print("""
+047) Crie um programa que mostre na tela todos os números pares que estão no intervalo
+entre 1 e 50.
+""")
+
+print('Lista dos números pares entre 1 e 50: ', end='')
+for numerosParesEntreUmECinquenta in range(2, 51, 2):
+    print(numerosParesEntreUmECinquenta, end=' ')
+print("""
+Fim!
+""")
