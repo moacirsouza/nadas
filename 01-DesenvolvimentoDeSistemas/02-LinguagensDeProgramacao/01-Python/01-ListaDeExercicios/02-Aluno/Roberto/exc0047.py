8bbe5c3cdc20612661a1ae453f71b1c6f1997499
robertolopesmendonca <robmen@gmail.com>
2020-03-25 22:46:46 -0300

    Feito...contador de pares !!

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Roberto/exc0047.py
@@ -0,0 +1,13 @@
+print("""
+047) Crie um programa que mostre na tela todos os números pares que estão no intervalo
+entre 1 e 50.
+""")
+print('Lista dos números pares entre 1 e 50: ')
+for numerospares in range (2, 51, 2):
+  print(numerospares, end=' ')
+print("""
+Fim!
+""")
+
+
+
