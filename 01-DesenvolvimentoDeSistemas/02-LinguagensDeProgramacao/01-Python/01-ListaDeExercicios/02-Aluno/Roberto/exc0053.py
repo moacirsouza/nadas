7fc7681622a3e2fcf1eb51592af5a4845fe166d2
robertolopesmendonca <robmen@gmail.com>
2020-03-27 08:53:34 -0300

    Palíndromo

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Roberto/exc0053.py
@@ -0,0 +1,32 @@
+print("""
+053) Crie um programa que leia uma frase qualquer e diga se ela é um
+políndromo, desconsiderando os espaços.
+""")
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto)-1, -1, -1):
+    inverso += junto[letra]
+print('O inverso de {} é {}'.format(junto, inverso))
+if inverso == junto:
+    print('Temos um palíndromo!')
+else:
+    print('A frase digitada não é um palíndromo!')
+print('Você digitou a frase {}'.format(junto))
+
+### Outro método, mais simples, sem uso do for !!
+
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = junto[::-1]
+print('O inverso de {} é {}'.format(junto, inverso))
+if inverso == junto:
+    print('Temos um palíndromo!')
+else:
+    print('A frase digitada não é um palíndromo!')
+print('Você digitou a frase {}'.format(junto))
+
+
+
