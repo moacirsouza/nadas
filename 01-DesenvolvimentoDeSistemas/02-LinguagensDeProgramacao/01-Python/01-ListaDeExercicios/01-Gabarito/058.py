e460c3c10028e5cd3d75c918f3af4200260bddbb
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-03-29 09:08:57 -0300

    Revisão da resolução do Exercício 058: Correções gramaticais no enunciado da questão.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/058.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/058.py
@@ -1,6 +1,6 @@
 print("""
 058) Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um número entre
-0 e 10. Só que agora o jogador vai tentar adivinhas até acertas, mostrando no final
+0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final
 quantos palpites foram necessários para vencer.
 """)
 
