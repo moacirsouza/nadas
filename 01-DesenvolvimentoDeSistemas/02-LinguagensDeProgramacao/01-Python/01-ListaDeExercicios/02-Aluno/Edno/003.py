f0c7da4ea8a7b0bdf845547a940ee0c77ff82cd3
Edno <edno.jr@hotmail.com>
2020-03-31 09:50:21 -0300

    Essa foi legal, Moka! Fiz atÃ© uma graÃa da saudosa Ãpoca quando os computadores demoravam que sÃ³pra dar um resultado. :D

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Edno/003.py
@@ -0,0 +1,39 @@
+# - [x] [003](01-Gabarito/003.py)) Crie um programa que leia dois números e mostre a soma entre eles.
+'''
+Projetinho pra calcular a soma entre dois números.
+Uma calculadora simples, mas bonitinha.
+'''
+
+from time import sleep
+
+print('*'*100)
+print('*' + ' Calculadora ' + ' '*85+'*')
+print('*'*100)
+numero1 = input('* Digite o primeiro número\t: ')
+numero2 = input('* Digite o segundo número\t: ')
+print('*'*100)
+print('')
+print('')
+print('*'*100)
+print('* Computando' + ' '*87 + '*')
+print('*'*100)
+sleep(0.5)
+print('')
+sleep(0.5)
+print('')
+sleep(0.5)
+print('')
+sleep(0.5)
+print('')
+
+print('*'*100)
+sleep(0.2)
+print('* Resultado' + ' '*87 + '*')
+sleep(0.2)
+print('*'*100)
+sleep(0.2)
+print('* A soma de ' + str(numero1) + ' + ' + str(numero2) + ' é igual a ' + str(int(numero1)+int(numero2)))
+sleep(0.2)
+print('*'*100)
+sleep(0.2)
+
