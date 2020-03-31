12515f2347a87dddb005c2c906af549ba257b977
Edno <edno.jr@hotmail.com>
2020-03-31 17:14:39 -0300

    Esse aÃ­ teve ideologia...

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Edno/013.py
@@ -0,0 +1,14 @@
+#(01-Gabarito/013.py)) Faça um algoritmo que leia o salário de um
+# funcionário e mostre seu novo salário, com 15% de aumento.
+#
+# Coisa de petista pra petista, aumentar 15% de salário. É, pq meu salário nunca subiu 5%.
+#:P:P:P
+
+salario = float(input('Qual seu salário, cumpanheiro? '))
+print('')
+print('Cumpanheiro, fica quieto, mas seu novo salário é ' + str(round(salario*1.15,2)))
+print('')
+print('Depois a gente vê a comissão.')
+
+
+
