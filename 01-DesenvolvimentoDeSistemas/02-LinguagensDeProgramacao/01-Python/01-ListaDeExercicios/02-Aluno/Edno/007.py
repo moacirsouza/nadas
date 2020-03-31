a8b5cebe04296b8deb83871b15d6e70c4deab3ba
Edno <edno.jr@hotmail.com>
2020-03-31 14:07:25 -0300

    Mission accomplished, sr!

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Edno/007.py
@@ -0,0 +1,18 @@
+# (01-Gabarito/007.py)) Desenvolva um programa que leia as duas notas de um aluno, calcula e mostre a sua média.
+'''
+O arquivo de James Bond!
+:D
+'''
+
+
+print('*'*100)
+print('* Eu sou o diretor, rapaz, e agora você está no juízo final!' + ' '*39 + '*')
+print('*'*100)
+print('*' + ' '*98 + '*')
+nota1 = float(input('* Qual é a sua primeira nota?\t'))
+nota2 = float(input('* Qual é a sua segunda nota?\t'))
+print('*' + ' '*98 + '*')
+print('* A sua média é ' + str((nota1+nota2)/2))
+print('*' + ' '*98 + '*')
+print('*'*100)
+print('ha, ha, ha, ha, ha!!!')
