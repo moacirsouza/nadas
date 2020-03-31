e7654ecc2a024c26b75477873f16b08f9d8356dc
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-03-30 23:55:16 -0300

    Resolução do Exercício 061.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/061.py
@@ -0,0 +1,21 @@
+print("""
+061) Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os
+10 primeiros termos da progressão usando a estrutura while.
+""")
+
+primeiroTermo = int(input('Informe o primeiro termo da PA: ').strip())
+razao = int(input('Informe a razão da PA: ').strip())
+
+termo = primeiroTermo
+contador = 0
+termosDaPA = ''
+
+while contador < 10:
+    contador += 1
+    termosDaPA += '{}, '.format(termo, end='')
+    termo += razao
+
+mensagemFinal = """
+Os termos da PA são: {}""".format(termosDaPA[:-2])
+
+print(mensagemFinal)
