79e1d95233439d9903f11a9e4ce48c91b5901484
robertolopesmendonca <robmen@gmail.com>
2020-04-03 16:33:32 -0300

    Fibonacci  OK !!!

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Roberto/exc0063.py
@@ -0,0 +1,24 @@
+print("""
+063) Escreva um programa que leia um  número n inteiro
+qualquer e mostre na tela os n primeiros elementos de uma Sequência de
+Fibonacci.
+Ex. 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8 
+""")
+
+print('-'*30)
+print('Sequência de Fibonacci')
+print('-'*30)
+n = int(input('Quantos termos você quer mostrar?'))
+t1 = 0
+t2 = 1
+print('~'*30)
+print('{} -> {}'.format(t1,t2), end='')
+cont = 3
+while cont <= n:
+    t3 = t1 + t2
+    print(' -> {}'.format(t3), end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print('-> FIM')
+print('~'*30)
