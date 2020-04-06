a41ac602c900dbe8b9b0138a5e25be017a5ec078
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-04-06 07:01:52 -0300

    Resolução do Exercício 066.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/066.py
@@ -0,0 +1,21 @@
+print("""
+066) Crie um programa que leia vários números inteiros pelo teclado. O programa só
+vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final
+mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando
+o flag).
+""")
+
+contador = soma = 0
+
+while True:
+    numero = int(input('Informe um número (999 para sair): ').strip())
+
+    if numero == 999:
+        break
+
+    soma += numero
+    contador += 1
+
+print("""
+{} números foram mostrados.
+A soma deles é {}""".format(contador, soma))
