f666d50f1fb4d7579c4889b49a926461de177d07
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-04-06 07:41:43 -0300

    Resolução do Exercício 067.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/067.py
@@ -0,0 +1,27 @@
+print("""
+067) Faça um programa que mostre a tabuada de vários números, um de cada vez, para
+cada valor digitado pelo usuário. O programa será interrompido quando o número
+solicitado for negativo.
+""")
+
+while True:
+    numero = int(input("""
+Quer ver a tabuada de qual número?
+Digite um número negativo para sair: """).strip())
+
+    if numero < 0:
+        break
+
+    ### Extração ipsis literis do Exercício 049,
+    ### que já imprime a tabuada de qualquer
+    ### número
+    titulo = 'Tabuada do ' + str(numero)
+
+    print('\n+{:-^16}+'.format(titulo))
+    for fatorMultiplicador in range(1, 11):
+        print('| {:>2} x {:>2} = {:>4} |'.format(numero,
+                                                fatorMultiplicador,
+                                                numero*fatorMultiplicador))
+    print('+'+('-'*16)+'+')
+
+print('Fim')
