c2e295f92013b6e3405ddead07026f4ebea754cc
Edno <edno.jr@hotmail.com>
2020-04-06 13:27:27 -0300

    Viajei geral na logica. Tava ateh indo bem, mas deu uma tontura! Resolvi arregar e pegar o gabarito. Me frustrei pq eu tava fazendo algo bem parecido. Enfim, Matematica 2, Edno 0

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Edno/063.py
@@ -0,0 +1,4 @@
+# (01-Gabarito/063.py)) Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n
+# primeiros elementos de uma Sequência de Fibonacci. Ex: 0 → 1 → 1 → 2 → 3 → 5 → 8
+
+
