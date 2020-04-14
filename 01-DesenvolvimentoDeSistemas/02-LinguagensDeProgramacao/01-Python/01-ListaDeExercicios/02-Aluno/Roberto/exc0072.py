9e573dfb002b89be9c9f09920e68316d593b6876
robertolopesmendonca <robmen@gmail.com>
2020-04-13 22:59:19 -0300

    72 - Tupla - OK

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Roberto/exc0072.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Roberto/exc0072.py
@@ -4,4 +4,13 @@ de zero até vinte.
 Seu programa deverá ler um número pelo teclado(entre 0 e 20) e 
 mostrá-lo por extenso.
 """)
-
+contagem = ('zero', 'um', 'dois', 'três', 'quatro',
+'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 
+'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 
+'dezoito', 'dezenove', 'vinte')
+while True:
+    número = int(input('Digite um  número entre 0 e 20: '))
+    if 0 <= número <=20:
+        break
+    print('Tente novamente. ', end='')
+print('Você digitou o número {}'.format(contagem[número]))
