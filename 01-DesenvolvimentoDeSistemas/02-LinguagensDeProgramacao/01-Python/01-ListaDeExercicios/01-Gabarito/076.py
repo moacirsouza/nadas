c0668d97c04fbb421323c2e1d6c33b2966745ad7
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-04-18 17:09:18 -0300

    Resolução do Exercício 076.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/076.py
@@ -0,0 +1,34 @@
+("""
+076) Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos
+preços, na sequência. No final, mostre uma listagem de preços organizando os dados em
+forma tabular.
+""")
+
+listaDeProdutos = ('Pão', 0.99, 'Manteiga', 2.5, 'Ovo (Bandeja com 30)', 6.2,
+                   'Maçã (Kg)', 8.5, 'Detergente de Pratos', 1.1, 'Sabonete', 0.85,
+                   'Água Sanitária', 123.2, 'Sabão em Pó', 6.5, 'Biscoito Bono', 1.85,
+                   'Batata Ruffles', 1.8, 'Fubá', 1.15, 'Café', 2.5)
+
+titulo = 'Lista de Preços'
+comprimentoDaTabela = 40
+contador = 0
+
+comprimentoDoMaiorPreco = len(str(max(listaDeProdutos[1::2])))
+print(comprimentoDoMaiorPreco)
+
+
+print('-'*comprimentoDaTabela)
+print(f'{titulo:^{comprimentoDaTabela}}')
+print('-'*comprimentoDaTabela)
+
+for contador in range(0, len(listaDeProdutos), 2):
+    produto = listaDeProdutos[contador]
+    preco = listaDeProdutos[contador+1]
+    
+    # TODO: Melhorar o cálculo do tamanho da tabela para adequar-se a qualquer
+    # valor informado na tupla.
+    quantidadeDePontos=comprimentoDaTabela-(comprimentoDoMaiorPreco+5)
+    
+    print(f'{produto:.<{quantidadeDePontos}}R$ {preco:>{comprimentoDoMaiorPreco+2},.2f}')
+
+print('-'*comprimentoDaTabela)
