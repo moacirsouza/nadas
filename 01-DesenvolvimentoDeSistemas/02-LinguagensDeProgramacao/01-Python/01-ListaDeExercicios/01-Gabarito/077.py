b0b0b9666e33ad1ba0fe1554f3327c192deff8a6
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-04-18 17:35:37 -0300

    Resolução do Exercício 077.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/077.py
@@ -0,0 +1,16 @@
+print("""
+077) Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois
+disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+""")
+
+listaDePalavras = ('mongoloide', 'egregios', 'assincrona', 'mitigar',
+                   'sincrona', 'confinamento', 'zaragatoa',
+                   'comorbidade', 'inferir', 'dicotomia', 'connosco',
+                   'inerente', 'moratoria', 'corroborar', 'conquanto')
+
+for palavra in listaDePalavras:
+    print(f'\nAs vogais da palavra {palavra.upper()}, são: ', end='')
+    for letra in palavra:
+        if letra in '[aeiou]':
+            print(letra, end=' ')
+print('')
