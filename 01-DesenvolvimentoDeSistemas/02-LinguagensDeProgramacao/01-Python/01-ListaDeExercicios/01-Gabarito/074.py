0260df89045d98ca8171929d7e2ea4bfbb2e5543
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-04-19 12:05:55 -0300

    Revisão da resolução do Exercício 074: Remoção do código que usa listas em favor de uma abordagem menos elegante, mas que não viola a regra de usar apenas conhecimentos ministrados no CeV até o momento da solicitação do exercício.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/074.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/074.py
@@ -5,12 +5,22 @@ valor que estão na tupla.
 """)
 
 from random import random
-listaTemporaria = []
 
-for contador in range(0,5):
-    listaTemporaria += [random()]
+### Solução 0: Essa é uma maneira mais elegante de resolver a questão, no
+### entanto, como combinado, tentaremos evitar usar conhecimentos que ainda
+### não foram ministrados nos módulos do Curso em Vídeo, portanto, o uso de
+### uma lista ainda não é permitido neste ponto.
+# listaTemporaria = []
+# for contador in range(0,5):
+#     listaTemporaria += [random()]
+# tuplaComCincoNumerosAleatorios = tuple(listaTemporaria)
 
-tuplaComCincoNumerosAleatorios = tuple(listaTemporaria)
+### Solução 1: A tupla é imutável, então, a única maneira de criar números
+### aleatórios nas cinco posições é chamar o método "random()" em cada uma
+### das posições individualmente, durante a declaração da variável.
+tuplaComCincoNumerosAleatorios = (random(), random(),
+                                  random(), random(),
+                                  random())
 
 print(f'A tupla gerada foi: {tuplaComCincoNumerosAleatorios}.')
 print(f'O maior número da tupla é {max(tuplaComCincoNumerosAleatorios)}.')
