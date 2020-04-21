c0095147bc4a299a21073d59942f242fdef3a4d0
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-04-20 21:54:28 -0300

    Resolução do Exercício 078.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/078.py
@@ -0,0 +1,29 @@
+print("""
+078) Faça um programa que leia 5 valores numéricos de guarde-os em uma lista. No final,
+mostre qual foi o maior e o menor valor digitado e suas respectivas posições na lista.
+""")
+
+listaComCincoValores = []
+
+for numero in range(1, 6):
+    listaComCincoValores += [int(input(f'Informe o {numero}º número: '))]
+
+maiorNumero = max(listaComCincoValores)
+menorNumero = min(listaComCincoValores)
+
+posicoesDosMenores = ''
+for posicao, numero in enumerate(listaComCincoValores):
+    if numero == menorNumero:
+        posicoesDosMenores += str(posicao) + ', '
+
+posicoesDosMaiores = ''
+for posicao, numero in enumerate(listaComCincoValores):
+    if numero == maiorNumero:
+        posicoesDosMaiores += str(posicao) + ', '
+
+print(f"""
+O menor número da lista é {menorNumero}.
+Ele pode ser encontrado na(s) posição(ões): {posicoesDosMenores[:-2]}.""")
+print(f"""
+O maior número da lista é {maiorNumero}.
+Ele pode ser encontrado na(s) posição(ões): {posicoesDosMaiores[:-2]}.""")
