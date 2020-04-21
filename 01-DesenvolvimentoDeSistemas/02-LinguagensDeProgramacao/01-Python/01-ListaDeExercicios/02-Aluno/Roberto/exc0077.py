2372cf318ea2c9e180615d081e690cbd75a02cb0
robertolopesmendonca <robmen@gmail.com>
2020-04-21 10:31:15 -0300

    77 OK !!

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Roberto/exc0077.py
@@ -0,0 +1,19 @@
+print("""
+077) Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois
+disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+""")
+
+palavras = ('aprender', 'programar', 'linguagem', 'python',
+'curso', 'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado',
+'programador', 'futuro')
+
+for p in palavras:
+    print('\nNa palavra {} temos '.format(p.upper()), end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
+
+
+
+
+
