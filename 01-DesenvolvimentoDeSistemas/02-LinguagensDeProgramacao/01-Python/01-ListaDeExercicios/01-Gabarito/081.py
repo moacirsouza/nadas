07b9d6126af673fe98b533473f2b3e8eca763fb3
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-04-23 07:40:49 -0300

    Resolução do Exercício 081.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/081.py
@@ -0,0 +1,33 @@
+print("""
+081) Crie um programa que vai ler vários números e colocar em uma lista. Depois disso,
+mostre:
+A) Quantos números foram digitados.
+B) A lista de valores, ordenada de forma decrescente.
+C) Se o valor 5 foi digitado e está ou não na lista.
+""")
+
+listaDeNumeros = []
+
+while True:
+    numero = int(input('Digite um número: ').strip())
+    listaDeNumeros.append(numero)
+
+    while True:
+        querContinuar = input('Deseja continuar? (s/n) ').strip().lower()
+
+        if querContinuar == 's' or querContinuar == 'n':
+            break
+        print('ATENÇÃO: Digite S/s ou N/n, respectivamente, para continuar ou sair do programa.')
+
+    if querContinuar == 'n':
+        listaDeNumeros.sort(reverse=True)
+
+        cincoEstaNaLista = 'O número 5 NÃO está na lista.'
+        
+        if 5 in listaDeNumeros:
+            cincoEstaNaLista = 'O número 5 está na lista.'
+        break
+
+print(f'A quantidade de números digitados foi {len(listaDeNumeros)}')
+print(f'A lista, em ordem decrescente, é: {listaDeNumeros}')
+print(f'{cincoEstaNaLista}')
