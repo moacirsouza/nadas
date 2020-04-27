42c2f7fa6b0a5a7753001f92d9bd1bd6cf95829b
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-04-26 22:05:27 -0300

    Revisao da resoluçao do Exercicio 082: Alteraçao dos textos dos comandos print a fim de deixar mais claro o ponto onde os erros eventualmente aconteceram

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/082.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/082.py
@@ -1,5 +1,5 @@
 print("""
-082) Crie um programa que vai ler vários números e colocar em uma lita. Depois disso,
+082) Crie um programa que vai ler vários números e colocar em uma lista. Depois disso,
 crie duas listas extras, que vão conter apenas os valores pares e os valores ímpares
 digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
 """)
