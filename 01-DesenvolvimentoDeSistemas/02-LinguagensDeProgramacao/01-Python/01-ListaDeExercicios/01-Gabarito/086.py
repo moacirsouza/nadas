6f2701f8c80e927f8bc1436d5bd0d5c3ff475747
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-04-27 22:38:58 -0300

    Resolução do Exercício 086.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/086.py
@@ -0,0 +1,26 @@
+print("""
+086) Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos
+pelo teclado. No final, mostre a matriz na tela com a formatação correta.
+""")
+
+tamanhoDaMatriz = 3
+matriz = []
+saida = ''
+titulo = f' Usando números inteiros preencha \
+a matriz {tamanhoDaMatriz}x{tamanhoDaMatriz} a seguir '
+
+print('-'*len(titulo))
+print(f'{titulo}')
+print('-'*len(titulo))
+
+for linha in range(tamanhoDaMatriz):
+    matriz.append([])
+    for coluna in range(tamanhoDaMatriz):
+        numero = int(input(f'Célula [{linha},{coluna}]: ').strip())
+        matriz[linha].append(numero)
+        saida += f'[ {matriz[linha][coluna]:^3} ]'
+    saida += '\n'
+
+print('-'*len(titulo))
+print(saida[:-1])
+print('-'*len(titulo))
