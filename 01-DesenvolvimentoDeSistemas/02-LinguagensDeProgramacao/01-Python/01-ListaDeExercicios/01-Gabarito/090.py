c3004d10290b6e71eba8b6174ab7e45f58de57e1
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-05-03 23:05:18 -0300

    Revisão da resolução do Exercício 090: Acréscimo de uma verificação para notas menores que 0 ou maiores que 10.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/090.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/090.py
@@ -5,6 +5,7 @@ em um dicionário. No final, mostre o conteúdo a estrutura na tela.
 
 nome = input('Informe o nome do(a) aluno(a): ').strip()
 media = float(input(f'Informe e a média de {nome}: ').strip())
+erro = 0
 
 if 7 <= media <= 10:
     situacao = 'Aprovado'
@@ -13,9 +14,14 @@ elif 5 <= media <= 6.9:
 elif 0 <= media < 5:
     situacao = 'Reprovado'
 else:
+    erro = 1
     situacao = 'Falha na entrada de dados. Procure a Adminstração da Escola.'
 
-aluno = {'Nome': nome, 'Média': media, 'Situação': situacao }
-
-for chave, valor in aluno.items():
-    print(f'{chave}: {valor}')
+if erro == 1:
+    print(situacao)
+else:
+    aluno = {'Nome': nome, 'Média': media, 'Situação': situacao }
+    print('-'*30)
+    for chave, valor in aluno.items():
+        print(f'{chave}: {valor}')
+    print('-'*30)
