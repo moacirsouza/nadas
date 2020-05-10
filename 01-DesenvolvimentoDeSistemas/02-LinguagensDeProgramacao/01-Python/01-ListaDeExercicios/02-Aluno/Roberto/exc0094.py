b5f4606f9e084554665afe9fe41afd74a60eb852
Roberto Mendonça <42980513+robertolopesmendonca@users.noreply.github.com>
2020-05-10 19:09:24 -0300

    94 Q Exercício difícil da gota serena !!!

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Roberto/exc0094.py
@@ -0,0 +1,48 @@
+print("""
+094) Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre:
+A) Quantas pessoas cadastradas
+B) A média de idade
+C) Uma lista com mulheres
+D) Uma lista com idade acima da média
+""")
+galera = list()
+pessoa = dict()
+soma = média = 0
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F. ')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N. ')
+    if resp == 'N':
+        break
+print('-= * 30')    
+print(galera)
+print(f'A) Ao todo temos {len(galera)} pessoas cadastradas.')
+média = soma / len(galera)
+print(f'B) A média de idade é de {média:5.2f} anos. ')
+print(' C) As mulheres cadastradas foram ', end='')
+for p in galera:
+    if p['sexo'] == 'F':
+        print(f'{p["nome"]} ', end='')
+print()
+print('D) Lista das pessoas que estão acima da média: ', end='')
+for p in galera:
+    if p['idade'] >= média:
+        print('    ')
+        for  k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('<< ENCERRADO >>')
+
+
