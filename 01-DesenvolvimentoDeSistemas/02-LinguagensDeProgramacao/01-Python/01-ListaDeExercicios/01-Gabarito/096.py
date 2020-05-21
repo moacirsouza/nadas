c0541944d84214b079502df939dc5a48ae907282
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-05-21 07:30:44 -0300

    Revisão da resolução do Exercício 096: Inclusão do recebimento das medidas do terrenos interativamente.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/096.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/096.py
@@ -5,7 +5,10 @@ de um terreno retangular (largura e comprimento) e mostre a área do terreno.
 
 def areaDeTerreno(largura, comprimento):
     area = largura*comprimento
-    print(f'A área do seu terreno é {area}m².')
+    print(f'A área do seu terreno é {area:.2f}m².')
 
 
-areaDeTerreno(4, 5)
+largura = float(input('Informe a largura do terreno em metros: ').strip())
+comprimento = float(input('Informe o comprimento do terreno em metros: ').strip())
+
+areaDeTerreno(largura, comprimento)
