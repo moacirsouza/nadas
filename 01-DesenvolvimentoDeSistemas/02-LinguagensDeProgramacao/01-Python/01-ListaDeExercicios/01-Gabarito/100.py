85f203f24f1780d6351a51e82f2955bad1510c4a
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-05-24 16:21:26 -0300

    Resolução do Exercício 100.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/100.py
@@ -0,0 +1,30 @@
+print("""
+100) Faça um programa que tenha uma lista chamada números e duas funções chamadas
+sorteia() e somaPar(). A primeira função vai sortear 5 números e vai colocá-los
+dentro da lista e a segunda função vai mostrar a soma entre todos os valores pares
+sorteados pela função anterior.
+""")
+
+from random import randint
+from time import sleep
+
+numeros = []
+
+def sorteia(lista):
+
+    print('Números sorteados: ')
+
+    for passo in range(5):
+        sorteado = randint(1, 10)
+        print(f'{passo+1}º: {sorteado} ')
+        sleep(0.5)
+        lista.append(sorteado)
+    print('')
+
+
+def somaPar(lista):
+    print(f'A soma dos números sorteados é {sum(lista)}')
+
+
+sorteia(numeros)
+somaPar(numeros)
