be30b11e110cc98562981cde72a26d2910515c20
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-05-24 15:56:52 -0300

    Resolução do Exercício 099.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/099.py
@@ -0,0 +1,12 @@
+print("""
+099) Faça um programa que tenha uma função chamada maior(), que receba vários
+parâmetros com valores inteiros. Seu programa tem que analisar todos os valores
+e dizer qual deles é o maior.
+""")
+
+def maior(*numeros):
+    print(f'O maior número entre {", ".join(map(str, numeros))}, é {max(numeros)}.')
+
+maior(2, 3)
+maior(1, 2, 3, 4)
+maior(6, 7, 8, 9)
