95bd1d95cadcda95f98f240bf662b5c805c7f1cd
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-03 07:03:18 -0300

    Criaçao do arquivo referente ao exercicio 107.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/107.py
@@ -0,0 +1,5 @@
+print("""
+107) Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(),
+diminuir(), dobro() e metade(). Faça também um programa que importe esse módulo e
+use algumas dessas funções.
+""")
