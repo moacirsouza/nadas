2cabcc81f27232bf704342144f235314229188ac
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-03 22:11:23 -0300

    Resoluçao do Exercicio 107.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/107.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/107.py
@@ -3,3 +3,23 @@ print("""
 diminuir(), dobro() e metade(). Faça também um programa que importe esse módulo e
 use algumas dessas funções.
 """)
+
+### Esse é o "truque" para importar modulos de um local fixo
+### Referência: https://do.co/2XuLGEN
+# import sys
+# sys.path.append('modulos')
+from modulos import moeda
+
+
+preco = 567
+incremento = 15
+decremento = 25
+precoIncrementado = moeda.aumentar(preco, incremento)
+precoDecrementado = moeda.diminuir(preco, decremento)
+precoDobrado = moeda.dobro(preco)
+precoPelaMetade = moeda.metade(preco)
+
+print(f'O valor {preco}, ao sofrer um aumento de {incremento}% fica igual a {precoIncrementado}.')
+print(f'O valor {preco}, ao sofrer uma reducao de {decremento}% fica igual a {precoDecrementado}.')
+print(f'O dobro do preço {preco} é {precoDobrado}.')
+print(f'A metade do valor {preco} é {precoPelaMetade}.')
