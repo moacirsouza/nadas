6c11a6b804fd6cdfe986e014572b4f7095e50657
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-03 22:44:19 -0300

    Resoluçao do Exercicio 108.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/108.py
@@ -0,0 +1,29 @@
+print("""
+108) Adapte o código do desafio #107, criando uma função adicional chamada moeda()
+que consiga mostrar os números como um valor monetário formatado.
+""")
+
+from modulos import moeda
+
+tamanhoDaBarra = 65
+preco = 567
+precoFormatado =  moeda.moeda(567)
+incremento = 15
+decremento = 25
+precoIncrementado = moeda.moeda(moeda.aumentar(preco, incremento))
+precoDecrementado = moeda.moeda(moeda.diminuir(preco, decremento))
+precoDobrado = moeda.moeda(moeda.dobro(preco))
+precoPelaMetade = moeda.moeda(moeda.metade(preco))
+
+
+print('-'*tamanhoDaBarra)
+print(f'{"Os valores informados foram":^{tamanhoDaBarra}}')
+print(f"""
+Preço: {precoFormatado}
+Taxa de incremento: {incremento}%
+Taxa de redução: {decremento}%""")
+print('-'*65)
+print(f'O valor {precoFormatado}, ao sofrer um aumento de {incremento}%, fica igual a {precoIncrementado}.')
+print(f'O valor {precoFormatado}, ao sofrer uma reducao de {decremento}%, fica igual a {precoDecrementado}.')
+print(f'O dobro do preço {precoFormatado} é {precoDobrado}.')
+print(f'A metade do valor {precoFormatado} é {precoPelaMetade}.')
