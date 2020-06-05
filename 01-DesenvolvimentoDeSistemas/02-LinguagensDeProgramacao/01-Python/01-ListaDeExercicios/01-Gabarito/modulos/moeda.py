03843c22ded6058bfcd685c2bf9a2515848cd454
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-05 07:48:38 -0300

    Revisao da resoluçao do modulo 'moeda.py': Incremento dos metodos 'aumentar()', 'diminuir()', 'dobro()' e 'metade()' para aceitar um parametro que indique que o retorno vai ser formatado como um valor monetario. Tambem foi adicionado ao metodo 'moeda()' um parametro que permite a indicacao de que formato deve ser usado na impressao do valor monetario. O metodo assume Real (R$), caso nenhum valor seja informado.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/modulos/moeda.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/modulos/moeda.py
@@ -1,23 +1,40 @@
-def aumentar(preco, taxa):
+def aumentar(preco=0, taxa=0, valorMonetario=False):
     retorno = preco*(1+taxa/100)
+
+    if valorMonetario:
+        retorno = moeda(retorno)
+
     return retorno
 
 
-def diminuir(preco, taxa):
+def diminuir(preco=0, taxa=0, valorMonetario=False):
     retorno = preco*(1-taxa/100)
+
+    if valorMonetario:
+        retorno = moeda(retorno)
+
     return retorno
 
 
-def dobro(preco):
+def dobro(preco=0, valorMonetario=False):
     retorno = preco*2
+
+    if valorMonetario:
+        retorno = moeda(retorno)
+
     return retorno
 
 
-def metade(preco):
+def metade(preco=0, valorMonetario=False):
     retorno = preco/2
+
+    if valorMonetario:
+        retorno = moeda(retorno)
+
     return retorno
 
 
-def moeda(preco):
-    retorno = f'R$ {preco:.2f}'
+def moeda(preco, formatadorDeMoeda='R'):
+    retorno = f'{formatadorDeMoeda}$ {preco:.2f}'
+    
     return retorno
