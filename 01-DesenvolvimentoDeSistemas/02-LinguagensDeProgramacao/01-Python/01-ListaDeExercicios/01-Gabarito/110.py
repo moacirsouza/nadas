43576a34b8f6fea4f381edd1aefc1ddfdf819463
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-05 08:40:56 -0300

    Resolucao do exercicio 110.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/110.py
@@ -0,0 +1,13 @@
+print("""
+110) Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada
+resumo(), que mostre na tela algumas informações geradas pelas funções que já temos
+no módulo criado até aqui.
+""")
+
+from modulos import moeda
+
+preco = float(input('Informe o preço: ').strip())
+aumento = float(input('Informe o acréscimo percentual: ').strip())
+reducao = float(input('Informe a reducao percentual: ').strip())
+
+moeda.resumo(preco, aumento, reducao)
