59e7c55bce4f39a6e7bb5f8709125ee23721d7e6
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-07 11:55:45 -0300

    Resolução do exercício 111.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/111.py
@@ -0,0 +1,8 @@
+print("""
+110) Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada
+resumo(), que mostre na tela algumas informações geradas pelas funções que já temos
+no módulo criado até aqui.
+""")
+
+print('Desde a aula 107 já utilizamos a estrutura de pacotes ao invés\
+dos módulos simples. Então, não tem o que fazer nesse exercício.')
