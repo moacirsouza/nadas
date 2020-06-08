7cd65545df60583984ba84cb28a9dbac4a4b55fb
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-07 22:57:39 -0300

    Resoluçao do Exercicio 112.

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/112.py
@@ -0,0 +1,12 @@
+print("""
+112) Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo
+chamado dado. Crie uma função chamada leiaDinheiro() que seja capaz de funcionar
+como a função imputa(), mas com uma validação de dados para aceitar apenas
+valores que seja monetários.
+""")
+
+from modulos import moeda
+from utilidadesCeV import dado
+
+preco = dado.leiaDinheiro('Informe o preço (R$): ')
+moeda.resumo(preco, 10, 15)
