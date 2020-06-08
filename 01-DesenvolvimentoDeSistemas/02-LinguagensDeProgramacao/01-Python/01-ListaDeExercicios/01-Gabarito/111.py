b5f112b80154f1fb62fca5165d30a87c4fdc7b9e
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-08 07:24:55 -0300

    Revisão da resolução do Exercício 111: Criação de uma chamada ao método 'moeda.resumo()', a fim de provar que tudo está funcionando normalmente.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/111.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/111.py
@@ -1,8 +1,13 @@
 print("""
-110) Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada
-resumo(), que mostre na tela algumas informações geradas pelas funções que já temos
-no módulo criado até aqui.
+111) Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados
+moeda e dado. Transfira todas as funções utilizadas nos desafios 107, 108 e 109 para
+o primeiro pacote e mantenha tudo funcionando.
 """)
 
-print('Desde a aula 107 já utilizamos a estrutura de pacotes ao invés\
-dos módulos simples. Então, não tem o que fazer nesse exercício.')
+print('Desde a aula 107 já utilizamos a estrutura de pacotes ao invés \
+dos módulos simples. Então, não tem o que fazer nesse exercício, a não \
+ser, uma chamada aos métodos já criados.')
+
+from modulos import moeda
+
+moeda.resumo(90, 10, 12)
