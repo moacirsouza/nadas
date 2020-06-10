923b4c499a8fb36b14ffa605db344c080c8e4097
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-10 11:03:07 -0300

    Resolução parcial do Exercício 115 e módulo "sistemaDeCadastro.py".

---
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/115.py
@@ -0,0 +1,14 @@
+print("""
+115) Crie um pequeno sistema modularizado que permita cadastrar pessoas pelo seu nome
+e idade em um arquivo de texto simples. O sistema só vai ter 2 opções: cadastrar uma
+nova pessoa e listar todas as pessoas cadastradas.
+""")
+
+#
+# ----------------------------
+#             Menu
+# ----------------------------
+# 1 - Ver pessoas cadastradas
+# 2 - Cadastrar nova pessoa
+# 3 - Sair do Sistema
+#
