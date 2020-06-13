6046f3e871e81ed9c7d76dfbe93a348401ec1f7f
Roberto Mendonça <42980513+robertolopesmendonca@users.noreply.github.com>
2020-06-13 11:50:47 -0300

    115b OK !!

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Roberto/Exc0115/sistema.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Roberto/Exc0115/sistema.py
@@ -1,10 +1,17 @@
 from lib.interface import *
+from lib.arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 while True:
     resposta = menu(['Ver Pessoas Cadastradas', 'Cadastrar Nova Pessoa',  'Sair do Sistema'])
     if resposta == 1:
-        cabeçalho('Opção 1')
+        # Opção de listar o conteúdo de um arquivo!
+        lerArquivo(arq)
     elif resposta == 2:
         cabeçalho('Opção 2')
     elif resposta == 3:
