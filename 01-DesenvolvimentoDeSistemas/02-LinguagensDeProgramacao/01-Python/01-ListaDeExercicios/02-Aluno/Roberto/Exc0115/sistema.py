17a957c1f6d7cfa080cc41483018fe059a8fccde
Roberto Mendonça <42980513+robertolopesmendonca@users.noreply.github.com>
2020-06-13 14:38:03 -0300

    105c OK !!

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Roberto/Exc0115/sistema.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/02-Aluno/Roberto/Exc0115/sistema.py
@@ -13,7 +13,11 @@ while True:
         # Opção de listar o conteúdo de um arquivo!
         lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho('Opção 2')
+        # Opção de cadastrar nova pessoa.
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabeçalho('Saindo do sistema ...Até logo!')
         break    
