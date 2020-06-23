b8414185cf1e4c975f00f3303380839347ff7c05
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-23 07:21:04 -0300

    Revisão da resolução do Exercício 007:  Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/007.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/007.py
@@ -1,7 +1,10 @@
 print("""
-007) Desenvolva um programa que leia as duas notas de um aluno, calcula e mostre a sua média. 
+007) Desenvolva um programa que leia as duas notas de um aluno, calcula e
+mostre a sua média. 
 """)
+
 nota01 = float(input('Informe a primeira nota do aluno: '))
 nota02 = float(input('Informe a primeira nota do aluno: '))
 media = (nota01+nota02)/2
+
 print('A média do aluno foi: {}'.format(media))
