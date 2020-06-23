10d0d19d170c07ff41cd75c20b3191f887517ee6
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-23 07:50:02 -0300

    Revisão da resolução do Exercício 027: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/027.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/027.py
@@ -1,5 +1,6 @@
 print("""
-027) Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+027) Faça um programa que leia o nome completo de uma pessoa, mostrando em
+seguida o primeiro e o último nome separadamente.
 Ex: Ana Maria de Souza.
 primeiro=Ana
 último: Souza.
