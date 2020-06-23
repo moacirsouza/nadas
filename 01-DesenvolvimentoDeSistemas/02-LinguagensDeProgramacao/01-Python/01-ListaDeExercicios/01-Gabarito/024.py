2fad68e93d3258846be5126cbbc6498cb4a3c131
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-23 07:45:02 -0300

    Revisão da resolução do Exercício 024: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/024.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/024.py
@@ -1,6 +1,12 @@
 print("""
-024) Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO". 
+024) Crie um programa que leia o nome de uma cidade e diga se ela começa
+ou não com o nome "SANTO".
 """)
-cidade = input('Digite o nome da cidade (Maiúsculas e Minúsculas não serão diferenciadas): ')
+
+cidade = input('Digite o nome da cidade (Maiúsculas e Minúsculas não serão \
+diferenciadas): ')
+
 buscaPeloSanto = cidade.lower().strip().startswith('santo')
-print('\nA cidade começa com a palavra SANTO? -> {}\n'.format(buscaPeloSanto))
+
+print('\nA cidade começa com a palavra SANTO? -> \
+{}\n'.format(buscaPeloSanto))
