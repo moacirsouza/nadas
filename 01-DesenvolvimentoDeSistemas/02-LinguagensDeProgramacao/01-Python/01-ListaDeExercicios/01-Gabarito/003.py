8f8b2b8e7b964ee8b0bf5122c4c3699d73812a06
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-23 07:14:52 -0300

    Revisão da resolução do Exercício 003: Revisão do limite máximo de colunas e organização do código para melhorar a legibilidade.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/003.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/003.py
@@ -1,8 +1,14 @@
 print("""
 003) Crie um programa que leia dois números e mostre a soma entre eles.
 """)
+
 numero01=int(input('Digite o número 01: '))
 numero02=int(input('Digite o número 02: '))
+
 soma=numero01+numero02
-print('[Colocando a soma diretamente no .format] A soma dos números é {}'.format(numero01+numero02))
-print('[Usando uma variável intermediária] A soma dos números é {}'.format(soma))
+
+print('\n[Colocando a soma diretamente no .format] A soma dos números é \
+{}'.format(numero01+numero02))
+
+print('\n[Usando uma variável intermediária] A soma dos números é \
+{}'.format(soma))
