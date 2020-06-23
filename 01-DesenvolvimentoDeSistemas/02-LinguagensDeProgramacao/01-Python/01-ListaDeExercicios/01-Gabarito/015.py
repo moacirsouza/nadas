8828954c3488763adc8c1ceebdc4eefa35720f66
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-23 07:31:22 -0300

    Revisão da resolução do Exercício 015: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/015.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/015.py
@@ -1,9 +1,17 @@
 print("""
-015) Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0.15 por Km rodado. 
+015) Escreva um programa que pergunte a quantidade de Km percorridos por um
+carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o
+preço a pagar, sabendo que o carro custa R$60 por dia e R$0.15 por Km rodado.
 """)
+
 diasDeLocacao = int(input('Por quantos dias o carro foi alugado? '))
 quilometrosRodados = float(input('Quantos quilômetros foram percorridos? '))
+
 custoDaLocacaoDiaria = 60
 custoPorQuilometroRodado = 0.15
-custoFinal = (diasDeLocacao*custoDaLocacaoDiaria) + (quilometrosRodados*custoPorQuilometroRodado)
-print('Um carro alugado por {} dia(s) e que rodou {} Km, custa um total de R$ {:.2f}'.format(diasDeLocacao,quilometrosRodados,custoFinal))
+
+custoFinal = (diasDeLocacao*custoDaLocacaoDiaria) + \
+(quilometrosRodados*custoPorQuilometroRodado)
+
+print('\nUm carro alugado por {} dia(s) e que rodou {} Km, custa um total de \
+R$ {:.2f}.'.format(diasDeLocacao,quilometrosRodados,custoFinal))
