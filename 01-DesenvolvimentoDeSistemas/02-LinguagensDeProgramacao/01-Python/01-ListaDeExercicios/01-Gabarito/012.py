fe885ae39c8b212630299e627c58f74f3a99b15b
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-23 07:26:50 -0300

    Revisão da resolução do Exercício 012: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/012.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/012.py
@@ -1,12 +1,18 @@
 print("""
-012) Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto. 
+012) Faça um algoritmo que leia o preço de um produto e mostre seu novo
+preço, com 5% de desconto. 
 """)
+
 precoDoProduto = float(input('Informe o preço do produto: R$ '))
 desconto = 0.05
+
 precoFinal = precoDoProduto-(precoDoProduto*desconto)
-print('O preço final do produto, com 5% de desconto, é R$ {:.2f}'.format(precoFinal))
+
+print('O preço final do produto, com 5% de desconto, é R$ \
+{:.2f}'.format(precoFinal))
 
 ### A fórmula a seguir é uma forma mais reduzida de calcular 5%
 ### de desconto de qualquer valor
-""" precoFinal = precoDoProduto*0.95
-print('O preço final do produto, com 5% de desconto, é R$ {:.2f}'.format(precoFinal)) """
+# precoFinal = precoDoProduto*0.95
+# print('O preço final do produto, com 5% de desconto, é R$ \
+# {:.2f}'.format(precoFinal))
