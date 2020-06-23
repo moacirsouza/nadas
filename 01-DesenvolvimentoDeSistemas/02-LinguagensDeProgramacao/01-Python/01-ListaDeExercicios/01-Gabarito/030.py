b2bcfc93f6357c64928ddde88766509e18b45fc7
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-23 07:55:43 -0300

    Revisão da resolução do Exercício 030: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/030.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/030.py
@@ -1,5 +1,6 @@
 print("""
-030) Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+030) Crie um programa que leia um número inteiro e mostre na tela se ele
+é PAR ou ÍMPAR.
 """)
 
 ### Entrada do programa: Recebe um número inteiro
