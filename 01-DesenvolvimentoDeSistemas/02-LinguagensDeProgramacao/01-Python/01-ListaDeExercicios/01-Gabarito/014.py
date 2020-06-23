e17071c6dbf47cbec077ba78fe024123507cac5a
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-23 07:29:26 -0300

    Revisão da resolução do Exercício 014: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/014.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/014.py
@@ -1,6 +1,11 @@
 print("""
-014) Escreva um programa que converta uma temperatura digitada em °C e converta para °F. 
+014) Escreva um programa que converta uma temperatura digitada em °C
+e converta para °F. 
 """)
-celsius = float(input('Digite a temperatura em Celsius (°C) para convertê-la em Fahrenheit (°F): '))
+
+celsius = float(input('Digite a temperatura em Celsius (°C) para \
+convertê-la em Fahrenheit (°F): '))
+
 fahrenheit = ((9*celsius)/5)+32 # Muitos parêntesis?
+
 print('{}°C equivalem a {}°F'.format(celsius,fahrenheit))
