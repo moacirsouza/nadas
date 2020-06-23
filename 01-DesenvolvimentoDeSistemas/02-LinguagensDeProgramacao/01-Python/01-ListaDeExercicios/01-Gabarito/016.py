403e0e08ca1d1084fe0d6f827c44b5cd065f98bf
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-23 07:32:08 -0300

    Revisão da resolução do Exercício 016: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/016.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/016.py
@@ -1,10 +1,15 @@
 print("""
-016) Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira. Ex: Digite um número: 6.127. O número 6.127 tem a parte inteira 6. 
+016) Crie um programa que leia um número Real qualquer pelo teclado e mostre
+na tela a sua porção Inteira. Ex: Digite um número: 6.127. O número 6.127
+tem a parte inteira 6. 
 """)
+
 ### A função trunc remove a parte inteira de um número real. Não é necessário
 ### importar toda a biblioteca math, apenas a função trunc. Isso economiza
 ### memória e deixa o código mais limpo.
 from math import trunc
+
 real = float(input('Digite um número real: '))
 parteInteira = trunc(real)
+
 print('A parte inteira do número {} é {}.'.format(real, parteInteira))
