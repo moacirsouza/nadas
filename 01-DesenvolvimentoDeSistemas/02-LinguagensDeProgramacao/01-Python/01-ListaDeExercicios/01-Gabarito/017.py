236aa9fac2c63e17c5577f5d3e4bd45965459eac
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-23 07:34:19 -0300

    Revisão da resolução do Exercício 017: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/017.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/017.py
@@ -1,18 +1,28 @@
 print("""
-017) Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa. 
+017) Faça um programa que leia o comprimento do cateto oposto e do cateto
+adjacente de um triângulo retângulo, calcule e mostre o comprimento da
+hipotenusa.
 """)
+
 from math import hypot, sqrt
-catetoOposto = float(input('Informe o valor do cateto oposto do triângulo retângulo: '))
-catetoAdjacente = float(input('Informe o valor do cateto adjacente do triângulo retângulo: '))
+
+catetoOposto = float(input('Informe o valor do cateto oposto do \
+triângulo retângulo: '))
+
+catetoAdjacente = float(input('Informe o valor do cateto adjacente do \
+triângulo retângulo: '))
 
 # Calculando a hipotenusa diretamente pela soma dos quadrados dos catetos
 hipotenusa = (catetoOposto**2+catetoAdjacente**2)**(1/2)
-print('Calculando a hipotenusa diretamente pela soma dos quadrados dos catetos: {}'.format(hipotenusa))
+print('Calculando a hipotenusa diretamente pela soma dos quadrados dos \
+catetos: {}'.format(hipotenusa))
 
 # Calculando a hipotenusa utilizando o método hypot do módulo math
 hipotenusa = hypot(catetoOposto, catetoAdjacente)
-print('Calculando a hipotenusa utilizando o método "hypot" da módulo "math": {}'.format(hipotenusa))
+print('Calculando a hipotenusa utilizando o método "hypot" da módulo "math": \
+{}'.format(hipotenusa))
 
 # Calculando a hipotenusa utilizando o método sqrt do módulo math
 hipotenusa = sqrt(catetoOposto**2+catetoAdjacente**2)
-print('Calculando a hipotenusa utilizando o método "sqrt" da módulo "math": {}'.format(hipotenusa))
+print('Calculando a hipotenusa utilizando o método "sqrt" da módulo "math": \
+{}'.format(hipotenusa))
