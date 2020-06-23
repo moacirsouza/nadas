e52e01cb145ac9a06651d0b83d64f12ff894683b
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-23 07:24:24 -0300

    Revisão da resolução do Exercício 010: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/010.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/010.py
@@ -1,7 +1,11 @@
 print("""
-010) Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar. Considere US$1,00=R$3,27 
+010) Crie um programa que leia quanto dinheiro uma pessoa tem na carteira
+e mostre quantos Dólares ela pode comprar. Considere US$1,00=R$3,27.
 """)
+
 reais = float(input('Quantos reais você tem? '))
 taxaDeCambio = 3.27
 dolares = reais/taxaDeCambio
-print('Com R$ {} e a cotação atual de US$ 1,00 = R$ {}, é possível comprar {:.2f} dólares.'.format(reais, taxaDeCambio, dolares))
+
+print('Com R$ {} e a cotação atual de US$ 1,00 = R$ {}, é possível comprar \
+{:.2f} dólares.'.format(reais, taxaDeCambio, dolares))
