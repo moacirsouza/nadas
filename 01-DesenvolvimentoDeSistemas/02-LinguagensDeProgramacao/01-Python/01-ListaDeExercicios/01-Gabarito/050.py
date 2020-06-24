cc9114e3e3cbd904c1a87b58c3258afc7686c120
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-24 07:56:46 -0300

    Revisão da resolução do Exercício 050: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/050.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/050.py
@@ -1,6 +1,7 @@
 print("""
-050) Desenvolva um programa que leia seis números inteiros e mostre a soma apenas
-daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o 
+050) Desenvolva um programa que leia seis números inteiros e mostre a
+soma apenas daqueles que forem pares. Se o valor digitado for ímpar,
+desconsidere-o.
 """)
 
 somaDosPares = 0
