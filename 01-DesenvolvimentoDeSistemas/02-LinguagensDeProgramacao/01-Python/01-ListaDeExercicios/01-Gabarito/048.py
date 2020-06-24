9751be9728a26acb3346852aae5badfa6006831f
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-24 07:53:08 -0300

    Revisão da resolução do Exercício 048: Revisão do limite máximo de colunas e alteração da maneira como a variável "mensagemFinal" é criada e impressa.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/048.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/048.py
@@ -1,13 +1,14 @@
 print("""
-048) Faça um programa que calcule a soma entre todos os números ímpares que são
-múltiplos de três e que se encontram no intervalo de 1 até 500.
+048) Faça um programa que calcule a soma entre todos os números ímpares que
+são múltiplos de três e que se encontram no intervalo de 1 até 500.
 """)
 
-mensagemFinal = 'A soma de todos os números é: {}'
 soma = 0
 
 for numero in range(1, 501, 2):
     if numero % 3 == 0:
         soma += numero
 
-print(mensagemFinal.format(soma))
+mensagemFinal = 'A soma de todos os números é: {}'.format(soma)
+
+print(mensagemFinal)
