6391070e051efae9e9eaf9bf30877349de8a98a2
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-24 07:54:19 -0300

    Revisão da resolução do Exercício 049: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/049.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/049.py
@@ -1,6 +1,6 @@
 print("""
-049) refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher,
-só que agora utilizando um laço for.
+049) refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário
+escolher, só que agora utilizando um laço for.
 """)
 
 numero = int(input('Digite um número inteiro: '))
