893d97353f10f9d93254260cc18d486d26bcccb1
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-24 07:27:45 -0300

    Revisão da resolução do Exercício 036: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/036.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/036.py
@@ -1,17 +1,21 @@
 print("""
-036) Escreva um programa para aprovar um empréstimo bancário para a compra de uma casa.
-O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele
-vai pagar.
+036) Escreva um programa para aprovar um empréstimo bancário para a compra
+de uma casa. O programa vai perguntar o valor da casa, o salário do comprador
+e em quantos anos ele vai pagar.
 
-Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou
-então o empréstimo será negado.
+Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do
+salário ou então o empréstimo será negado.
 """)
 
-### Entrada do programa: Recebe o valor da casa e o salário do comprador como número real
-### ("float") e o tempo de pagamento como inteiro ("int").
+### Entrada do programa: Recebe o valor da casa e o salário do comprador como
+### número real ("float") e o tempo de pagamento como inteiro ("int").
 valorDoImovel = float(input('Informe o valor do imóvel: R$ ').strip())
-salarioDoComprador = float(input('Informe o salário do(a) comprador(a): R$ ').strip())
-tempoDePagamento = int(input('Informe o tempo de pagamento (Em anos): ').strip())
+
+salarioDoComprador = float(input('Informe o salário do(a) comprador(a): \
+R$ ').strip())
+
+tempoDePagamento = int(input('Informe o tempo de pagamento \
+(Em anos): ').strip())
 
 percentualDoSalario = 0.3
 limiteSuperior = salarioDoComprador*percentualDoSalario
