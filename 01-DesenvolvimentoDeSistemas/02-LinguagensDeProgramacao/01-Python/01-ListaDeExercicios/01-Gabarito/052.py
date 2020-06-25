82e675e87177bab49135612f55870e150a992c6f
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-25 07:12:37 -0300

    Revisão da resolução do Exercício 052: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/052.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/052.py
@@ -1,8 +1,11 @@
 print("""
-052) Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+052) Faça um programa que leia um número inteiro e diga se ele é ou não
+um número primo.
 """)
 
-numero = int(input('Digite um número para verificar se ele é primo: ').strip())
+numero = int(input('Digite um número para verificar se \
+ele é primo: ').strip())
+
 contador = 0
 nao = ' não'
 
