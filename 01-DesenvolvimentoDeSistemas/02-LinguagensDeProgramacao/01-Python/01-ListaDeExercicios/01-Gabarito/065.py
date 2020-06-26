bada06eaf61a172a2b0962eb26fc8d216a7bbe09
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-26 07:16:11 -0300

    Revisão da resolução do Exercício 065: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/065.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/065.py
@@ -1,7 +1,8 @@
 print("""
-065) Crie um programa que leia vários números inteiros pelo teclado. No final da execução
-mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa
-deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+065) Crie um programa que leia vários números inteiros pelo teclado.
+No final da execução mostre a média entre todos os valores e qual foi
+o maior e o menor valores lidos. O programa deve perguntar ao usuário
+se ele quer ou não continuar a digitar valores.
 """)
 
 maior = menor = soma = contador = 0
