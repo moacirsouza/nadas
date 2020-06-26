309af042e701347125c2368a3cb568b686fcac0d
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-26 07:13:15 -0300

    Revisão da resolução do Exercício 063: Revisão do limite máximo de colunas.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/063.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/063.py
@@ -1,8 +1,7 @@
 print("""
-063) Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n
-primeiros elementos de uma Sequência de Fibonacci.
-Ex:
-0 → 1 → 1 → 2 → 3 → 5 → 8 
+063) Escreva um programa que leia um número n inteiro qualquer e mostre
+na tela os n primeiros elementos de uma Sequência de Fibonacci.
+Ex: 0 → 1 → 1 → 2 → 3 → 5 → 8 
 """)
 
 ### A Sequênia de Fibonacci é definida como: 
@@ -16,10 +15,15 @@ Ex:
 ### https://oeis.org/A000045
 
 contador = 2
-quantidadeDeTermos = int(input('Quantos termos da Sequência de Fibonacci você deseja mostrar: ').strip())
+
+quantidadeDeTermos = int(input('Quantos termos da Sequência de Fibonacci \
+você deseja mostrar: ').strip())
+
 fibonacci = [0, 1]
 
 while contador < quantidadeDeTermos:
     fibonacci += [fibonacci[contador-1]+fibonacci[contador-2]]
     contador += 1
-print('A sequência de Fibonacci com {} termos é:\n{}'.format(contador, fibonacci))
+
+print('A sequência de Fibonacci com \
+{} termos é:\n{}'.format(contador, fibonacci))
