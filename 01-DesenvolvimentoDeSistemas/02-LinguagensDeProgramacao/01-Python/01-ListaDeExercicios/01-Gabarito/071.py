0d7cc587b1c035497f14f828e27672c252643879
Moacir da Cruz Souza Filho <moacir.souza@gmail.com>
2020-06-29 07:29:17 -0300

    Revisão da resolução do Exercício 071: Revisão do limite máximo de colunas. Acréscimo da solução 3, desenvolvida pelo colega Emanoel Delfinom, como sugestão de melhoria da lógica e elegância do código.

--- 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/071.py
+++ 01-DesenvolvimentoDeSistemas/02-LinguagensDeProgramacao/01-Python/01-ListaDeExercicios/01-Gabarito/071.py
@@ -1,13 +1,15 @@
 print("""
-071) Crie um programa que simule o funcionamento de um caixa eletrônico. No início, 
-pergunte ao usuário qual será o valor a ser sacado (Número inteiro) e o programa vai
-informar quantas cédulas de cada valor serão entregues.
-OBS: Considere que o Caixa possui cédulas de R$ 50, R$ 20, R$ 10 e R$ 1.
+071) Crie um programa que simule o funcionamento de um caixa eletrônico.
+No início, pergunte ao usuário qual será o valor a ser sacado (Número
+inteiro) e o programa vai informar quantas cédulas de cada valor serão
+entregues.
+OBS.: Considere que o Caixa possui cédulas de R$ 50, R$ 20, R$ 10 e R$ 1.
 """)
 
 ### Solução 0: Funcional, mas excessivamente complicada.
 # valorDoSaque = []
-# valorDoSaque.extend(input('Quanto quer sacar? ').strip()) # Mantido como "string" para aproveitar a propriedade de iterável do objeto
+# valorDoSaque.extend(input('Quanto quer sacar? ').strip()) # Mantido como \
+# "string" para aproveitar a propriedade de iterável do objeto
 # multiplicador = 1
 # notas = ''
 # while len(valorDoSaque) != 0:
@@ -15,9 +17,12 @@ OBS: Considere que o Caixa possui cédulas de R$ 50, R$ 20, R$ 10 e R$ 1.
 #     # Validação da unidade:
 #     if multiplicador == 1:
 #         if valor < 5:
-#             notas += str(valor) + ' nota(s) de 1.\n'
+#             notas += str(valor) \
+#                      + ' nota(s) de 1.\n'
 #         elif valor > 5:
-#             notas += '1 nota de 5 e ' + str(valor%5) + ' nota(s) de 1.\n'
+#             notas += '1 nota de 5 e ' \
+#                      + str(valor%5) \
+#                      + ' nota(s) de 1.\n'
 #         else:
 #             notas += '1 nota de 5.\n'
 #     # Validação da dezena
@@ -26,16 +31,24 @@ OBS: Considere que o Caixa possui cédulas de R$ 50, R$ 20, R$ 10 e R$ 1.
 #             notas += '1 nota de 10.\n'
 #         elif 5 > valor >= 2:
 #             if valor%2 == 0:
-#                 notas += str(valor//2) + ' nota(s) de 20.\n'
+#                 notas += str(valor//2) + \
+#                          ' nota(s) de 20.\n'
 #             else:
-#                 notas += str(valor//2) + ' nota(s) de 20 e ' + str(valor%2) + ' nota(s) de 10\n'
+#                 notas += str(valor//2) \
+#                          + ' nota(s) de 20 e ' \
+#                          + str(valor%2) \
+#                          + ' nota(s) de 10\n'
 #         elif valor > 5:
 #             notas += '1 nota de 50 e '
 #             if valor%5 >= 2:
 #                 if (valor%5)%2 == 0:
-#                     notas += str((valor%5)//2) + ' nota(s) de 20.\n'
+#                     notas += str((valor%5)//2) \
+#                              + ' nota(s) de 20.\n'
 #                 else:
-#                     notas += str((valor%5)//2) + ' nota(s) de 20 e ' + str((valor%5)//2) + ' nota(s) de 10\n'
+#                     notas += str((valor%5)//2) \
+#                              + ' nota(s) de 20 e ' \
+#                              + str((valor%5)//2) \
+#                              + ' nota(s) de 10\n'
 #             else:
 #                 notas += '1 nota de 10.\n'
 #         else:
@@ -47,8 +60,9 @@ OBS: Considere que o Caixa possui cédulas de R$ 50, R$ 20, R$ 10 e R$ 1.
 #     multiplicador *= 10
 # print(notas)
 
-### Solução 1: Mais simples que a Solução 0, mas muito deselegante. Usa quatro
-### contadores, cria muita repetição de código. Um desastre :)
+### Solução 1: Mais simples que a Solução 0, mas muito deselegante.
+### Usa quatro contadores, cria muita repetição de código. Um
+### desastre :)
 # valorDoSaque = int(input('Quanto quer sacar? '))
 # contador50 = contador20 = contador10 = contador1 = 0
 # resultado = ''
@@ -82,45 +96,72 @@ OBS: Considere que o Caixa possui cédulas de R$ 50, R$ 20, R$ 10 e R$ 1.
 # print(resultado)
 
 ### Solução 2: A mais simples e elegante de todas.
-valorDoSaque = int(input('Quanto quer sacar? R$ '))
-cedula = 50
-contador = 0
+# valorDoSaque = int(input('Quanto quer sacar? R$ '))
+# cedula = 50
+# contador = 0
+# while True:
+#     ### [1] O teste só falha quando o valor do saque for menor que o valor
+#     ### da cédula que está sendo usada na iteração atual. Ou seja, quando
+#     ### a subtração der resultado negativo, significa que é preciso "trocar"
+#     ### para a cédula de valor imediatamente inferior, até zerar a variável
+#     ### "valorDoSaque", usando as cédulas de R$ 1,00.
+#     if valorDoSaque >= cedula:
+#         contador += 1
+#         valorDoSaque -= cedula
+#     else:
+#         ### [3] A variável "contador" indica a quantidade de cédulas de cada
+#         ### valor a cada iteração. Se este valor for 0, significa que não
+#         ### houve contabilização daquela cédula para compor o saque. Como
+#         ### não é preciso indicar que zero cédulas de um determinado valor
+#         ### serão entregues, só realizamos esta ação, caso contador > 0
+#         if contador > 0:
+#             print(f'{contador} cédula(s) de {cedula}')
+#
+#         ### Apenas quando "valorDoSaque" for zero, a operação termina.
+#         if valorDoSaque == 0:
+#             break
+#
+#         ### [2] Como elucidado no comentário [1], uma vez que o "else" foi
+#         ### alcançado, é preciso alterar o valor da cédula para aquele
+#         ### imediatamente inferior, segundo a definição da Regra de
+#         ### Negócios. O conjunto de testes a seguir atinge esse propósito.
+#         ### Seja qual for o valor da cédula da iteração atual, uma das
+#         ### estruturas de controle vai capturá-lo e alterá-lo para próximo.
+#         if cedula == 50:
+#             cedula = 20
+#         elif cedula == 20:
+#             cedula = 10
+#         elif cedula == 10:
+#             cedula = 1
+#
+#         ### Depois de alterar o valor da cédula, o contaor precisa
+#         ### ser zerado, caso contrário, ele acumulará o total do
+#         ### grupo anterior de cédulas ao grupo da iteração atual
+#         contador = 0
+
+### Solução 3: Desenvolvida pelo colega Emanoel Delfino, entrega uma lógica
+### mais elegante em um código enxuto e matematicamente preciso.
+### Referência: https://bit.ly/2CKLxVG
+print('=' * 35)
+print(f'|{"BANCO DO BRASIL":^33}|')
+print('=' * 35)
+valor = int(input('Qual valor deseja sacar? R$'))
+print('=' * 35)
+
+cedulas = [50, 20, 10, 1]
+
+c = 0
+
 while True:
-    ### [1] O teste só falha quando o valor do saque for menor que o valor da
-    ### cédula que está sendo usada na iteração atual. Ou seja, quando a
-    ### subtração der resultado negativo, significa que é preciso "trocar" para
-    ### a cédula de valor imediatamente inferior, até zerar a variável
-    ### "valorDoSaque", usando as cédulas de R$ 1,00.
-    if valorDoSaque >= cedula:
-        contador += 1
-        valorDoSaque -= cedula
-    else:
-        ### [3] A variável "contador" indica a quantidade de cédulas de cada
-        ### valor a cada iteração. Se este valor for 0, significa que não houve
-        ### contabilização daquela cédula para compor o saque. Como não é
-        ### preciso indicar que zero cédulas de um determinado valor serão
-        ### entregues, só realizamos esta ação, caso contador > 0
-        if contador > 0:
-            print(f'{contador} cédula(s) de {cedula}')
+    if valor >= cedulas[c]:
+        cedula = cedulas[c]
+        totcedulas = valor // cedula
+        print(f'Total de {totcedulas:2} cédula(s) de R${cedula:.2f}')
+        valor %= cedula
+    if valor == 0:
+        break
+    c += 1
 
-        ### Apenas quando "valorDoSaque" for zero, a operação termina.
-        if valorDoSaque == 0:
-            break
-
-        ### [2] Como elucidado no comentário [1], uma vez que o "else" foi
-        ### alcançado, é preciso alterar o valor da cédula para aquele
-        ### imediatamente inferior, segundo a definição da Regra de Negócios.
-        ### O conjunto de testes a seguir atinge esse propósito. Seja qual for
-        ### o valor da cédula da iteração atual, uma das estruturas de controle
-        ### vai capturá-lo e alterá-lo para próximo.
-        if cedula == 50:
-            cedula = 20
-        elif cedula == 20:
-            cedula = 10
-        elif cedula == 10:
-            cedula = 1
-
-        ### Depois de alterar o valor da cédula, o contaor precisa ser zerado,
-        ### caso contrário, ele acumulará o total do grupo anterior de cédulas
-        ### ao grupo da iteração atual
-        contador = 0
+print('=' * 35)
+print('Volte sempre ao BANCO DO BRASIL!\nTenha um bom dia!')
+print('=' * 35)
